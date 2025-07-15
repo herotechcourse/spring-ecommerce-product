2876727d8b3db3ddfc2d0944410cb0e92c5dbc6e
Mina Kim <mina.kim@deliveryhero.com>
2025-07-15 16:47:20 +0200

    fix(controller): change path to follow mission requirement

--- src/main/kotlin/ecommerce/product/ProductController.kt
+++ src/main/kotlin/ecommerce/product/ProductController.kt
@@ -2,9 +2,7 @@ package ecommerce.product
 
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.*
 import java.net.URI
 import java.util.concurrent.atomic.AtomicLong
 
@@ -13,14 +11,14 @@ class ProductController {
     private val products: MutableList<Product> = ArrayList()
     private val index = AtomicLong(1)
 
-    @PostMapping("/products")
+    @PostMapping("/api/products")
     fun create(@RequestBody product: Product): ResponseEntity<Product> {
-       val newProduct = Product.toEntity(product, index.getAndIncrement())
+        val newProduct = Product.toEntity(product, index.getAndIncrement())
         products.add(newProduct)
         return ResponseEntity.created(URI.create("/products/" + newProduct.id)).body(product)
     }
 
-    @GetMapping("/products")
+    @GetMapping("/api/products")
     fun read(): ResponseEntity<List<Product>> {
         return ResponseEntity.ok().body(products)
     }
