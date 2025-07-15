16c3cf2e5c6b6a1110e088e0f6d8216d67539e82
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-15 19:21:22 +0200

    test: refactor tests

--- src/test/kotlin/ecommerce/controller/ProductControllerTest.kt
+++ src/test/kotlin/ecommerce/controller/ProductControllerTest.kt
@@ -4,44 +4,48 @@ import ecommerce.model.Product
 import io.restassured.RestAssured
 import io.restassured.http.ContentType
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpStatus
+import org.springframework.test.annotation.DirtiesContext
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class ProductControllerTest {
-    companion object {
-        fun createProducts(): MutableSet<Product> {
-            val product1 = Product(1L, "cafe", 39.00, "www.test")
-            val product2 = Product(2L, "table", 39.00, "www.test")
-            val product3 = Product(3L, "chair", 39.00, "www.test")
-            val products =
-                RestAssured
-                    .given().log().all().body(product1)
-                    .contentType(ContentType.JSON)
-                    .`when`()
-                    .request("POST", "/products")
-                    .then()
-                    .extract()
-                    .response()
+    private lateinit var products: Set<Product>
+
+    @BeforeEach
+    fun createProducts() {
+        val product1 = Product(1L, "cafe", 39.00, "www.test")
+        val product2 = Product(2L, "table", 39.00, "www.test")
+        val product3 = Product(3L, "chair", 39.00, "www.test")
+        val productsExpected =
             RestAssured
-                .given().log().all().body(product2)
+                .given().log().all().body(product1)
                 .contentType(ContentType.JSON)
                 .`when`()
                 .request("POST", "/products")
                 .then()
                 .extract()
                 .response()
-            RestAssured
-                .given().log().all().body(product3)
-                .contentType(ContentType.JSON)
-                .`when`()
-                .request("POST", "/products")
-                .then()
-                .extract()
-                .response()
-            return mutableSetOf(product1, product2, product3)
-        }
+        RestAssured
+            .given().log().all().body(product2)
+            .contentType(ContentType.JSON)
+            .`when`()
+            .request("POST", "/products")
+            .then()
+            .extract()
+            .response()
+        RestAssured
+            .given().log().all().body(product3)
+            .contentType(ContentType.JSON)
+            .`when`()
+            .request("POST", "/products")
+            .then()
+            .extract()
+            .response()
+        products = mutableSetOf(product1, product2, product3)
     }
 
     @Test
@@ -65,7 +69,6 @@ class ProductControllerTest {
 
     @Test
     fun getProducts() {
-        val products = createProducts()
         val response =
             RestAssured
                 .given().log().all()
@@ -82,7 +85,6 @@ class ProductControllerTest {
     @Test
     fun getProduct() {
         val product = Product(1L, "cafe", 39.00, "www.test")
-        val products = createProducts()
         val response =
             RestAssured
                 .given().log().all()
