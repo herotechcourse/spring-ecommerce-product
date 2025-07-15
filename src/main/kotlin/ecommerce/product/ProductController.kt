3bc85a405cd41bb515e76accb558420f42412a8a
Mina Kim <mina.kim@deliveryhero.com>
2025-07-15 18:43:51 +0200

    refactor(controller): change products data struct to a mutable map in ProductController
    - products: MutableMap<Long, Product>

--- src/main/kotlin/ecommerce/product/ProductController.kt
+++ src/main/kotlin/ecommerce/product/ProductController.kt
@@ -13,21 +13,23 @@ import java.util.concurrent.atomic.AtomicLong
 
 @Controller
 class ProductController {
-    private val products: MutableList<Product> = ArrayList()
+    private val products: MutableMap<Long, Product> = HashMap()
     private val index = AtomicLong(1)
 
     @PostMapping("/api/products")
     fun create(
         @RequestBody product: Product,
     ): ResponseEntity<Product> {
-        val newProduct = Product.toEntity(product, index.getAndIncrement())
-        products.add(newProduct)
+        val id = index.getAndIncrement()
+        val newProduct = Product.toEntity(product, id)
+
+        products.put(id, newProduct)
         return ResponseEntity.created(URI.create("/products/" + newProduct.id)).body(product)
     }
 
     @GetMapping("/api/products")
     fun read(): ResponseEntity<List<Product>> {
-        return ResponseEntity.ok().body(products)
+        return ResponseEntity.ok(products.values.toList())
     }
 
     @PutMapping("/api/products/{id}")
@@ -35,11 +37,8 @@ class ProductController {
         @RequestBody newProduct: Product,
         @PathVariable id: Long,
     ): ResponseEntity<Product> {
-        val product =
-            products.stream()
-                .filter { it.id == id }
-                .findFirst()
-                .orElseThrow { RuntimeException("Product with id $id not found") }
+        val product = products.getValue(id)
+
         product.update(newProduct)
         return ResponseEntity.ok().body(product)
     }
@@ -48,12 +47,8 @@ class ProductController {
     fun delete(
         @PathVariable id: Long,
     ): ResponseEntity<Void> {
-        val product =
-            products.stream()
-                .filter { it.id == id }
-                .findFirst()
-                .orElseThrow { RuntimeException("Product with id $id not found") }
-        products.remove(product)
+        products.getValue(id)
+        products.remove(id)
         return ResponseEntity.noContent().build()
     }
 }
