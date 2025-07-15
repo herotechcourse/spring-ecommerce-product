6fc23a9062d0705666a443487175f2a578ff40d2
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-15 17:40:34 +0200

    feat: implement deleteProduct endpoint

--- src/main/kotlin/ecommerce/controller/ProductController.kt
+++ src/main/kotlin/ecommerce/controller/ProductController.kt
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
@@ -44,5 +45,16 @@ class ProductController {
         val product =
             products.first { it.id == id }
         return ResponseEntity.ok(product)
+    }
+
+    @PutMapping("/product/{id}")
+    fun updateProduct(
+        @PathVariable id: Long,
+        @RequestBody product: Product,
+    ): ResponseEntity<Product> {
+        products.removeIf { it.id == id }
+        val newProduct = Product(id, product.name, product.price, product.imageUrl)
+        products.add(newProduct)
+        return ResponseEntity.ok(newProduct)
     }
 }
