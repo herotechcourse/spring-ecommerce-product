ef839696494fefbbc9dd856818279cd7f5e65b1e
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-15 17:19:42 +0200

    feat: implement getProduct endpoint

--- src/main/kotlin/ecommerce/controller/ProductController.kt
+++ src/main/kotlin/ecommerce/controller/ProductController.kt
@@ -3,18 +3,20 @@ package ecommerce.controller
 import ecommerce.model.Product
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
-import java.util.HashMap
 import java.util.concurrent.atomic.AtomicLong
 
 @Controller
 @RequestMapping
 class ProductController {
-    private val products: MutableMap<Long, Product> = HashMap()
-    private var index: AtomicLong = AtomicLong(0)
+    private val products: MutableSet<Product> = HashSet()
+
+    private var index: AtomicLong = AtomicLong(1)
 
     @PostMapping("/product", consumes = ["application/json"])
     @ResponseBody
@@ -22,10 +24,7 @@ class ProductController {
         @RequestBody product: Product,
     ): Product {
         val product = Product(index.getAndAdd(1), product.name, product.price, product.imageUrl)
-        products.put(
-            key = products.size.toLong() + 1,
-            value = product,
-        )
+        products.add(product)
         return product
     }
 
@@ -34,10 +33,16 @@ class ProductController {
         @RequestBody product: Product,
     ): ResponseEntity<Product> {
         val product = Product(product.id, product.name, product.price, product.imageUrl)
-        products.put(
-            key = 1,
-            value = product,
-        )
+        products.add(product)
+        return ResponseEntity.ok(product)
+    }
+
+    @GetMapping("/product/{id}")
+    fun getProduct(
+        @PathVariable id: Long,
+    ): ResponseEntity<Product> {
+        val product =
+            products.first { it.id == id }
         return ResponseEntity.ok(product)
     }
 }
