8a5a4550979ea473086bfd6ccb8cc0c9be8e9e4d
Bojana Petroska <bojana.petroska@yahoo.com>
2025-07-16 15:49:25 +0200

    feat(product): add ProductView
    - created a template to list all the available products

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # spring-ecommerce-product
 
+## Product
   -[x] Create a simple CRUD HTTP API
 - [x] Create a Product
 - [x] Retrieve a Product
@@ -7,3 +8,8 @@
 - [x] Update a Product
 - [x] Delete a product
 - [x] Tests for CRUD operations
+
+## Admin
+- [ ] Login for Admin
+- [ ] Admin can view, add, update, and delete products
+- [x] Template for Product Page
---
+++ src/main/kotlin/ecommerce/controller/ProductViewController.kt
@@ -0,0 +1,22 @@
+package ecommerce.controller
+
+import ecommerce.model.Product
+import ecommerce.service.ProductService
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class ProductViewController(private val productService: ProductService) {
+    @GetMapping("/")
+    fun welcomePage(): String {
+        return "/products"
+    }
+
+    @GetMapping("/admin/products")
+    fun listAllProducts(model: Model): String {
+        model.addAttribute("products", productService.getAllProducts())
+        model.addAttribute("product", ::Product)
+        return "products"
+    }
+}
