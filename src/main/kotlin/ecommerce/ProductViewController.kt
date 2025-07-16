18504ddca48f77a8a9268cf221e00a0c55f27134
Sanghun Park <po4865@gmail.com>
2025-07-16 15:08:02 +0200

    feat(ProductViewController): create ProductViewController to show products page
    - add products.html file as template in resource

---
+++ src/main/kotlin/ecommerce/ProductViewController.kt
@@ -0,0 +1,20 @@
+package ecommerce
+
+import ecommerce.api.ProductController
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class ProductViewController(
+    private val controller: ProductController,
+) {
+    @GetMapping("/")
+    fun displayProducts(model: Model): String {
+        // TODO: Figure out how to deal with controller connection to get data
+        val products = controller.products.values.toList()
+
+        model.addAttribute("products", products)
+        return "products"
+    }
+}
