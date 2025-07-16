db30cbf2f90ad961edd55cfdb96222fbf04a991a
Sanghun Park <po4865@gmail.com>
2025-07-16 15:11:41 +0200

    feat(ProductViewController): add displayUpdateProductForm() function to provide view with form for updating product
    - add updateProductForm.html file as template in resource

--- src/main/kotlin/ecommerce/ProductViewController.kt
+++ src/main/kotlin/ecommerce/ProductViewController.kt
@@ -4,6 +4,7 @@ import ecommerce.api.ProductController
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
 class ProductViewController(
@@ -21,5 +22,15 @@ class ProductViewController(
     @GetMapping("/add/product")
     fun displayCreateProductForm(): String {
         return "createProductForm"
+    }
+
+    @GetMapping("/update/product/{id}")
+    fun displayUpdateProductForm(
+        @PathVariable id: Long,
+        model: Model,
+    ): String {
+        val product = controller.products[id]
+        model.addAttribute("product", product)
+        return "updateProductForm"
     }
 }
