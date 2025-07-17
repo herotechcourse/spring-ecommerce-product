f9c64fd4e1e10a7f4c96ea9e383dd87de67cb527
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-17 16:16:50 +0200

    refactor: use model.addAttribute to refill the product list

--- src/main/kotlin/ecommerce/controller/ProductController.kt
+++ src/main/kotlin/ecommerce/controller/ProductController.kt
@@ -5,6 +5,7 @@ import ecommerce.repository.ProductRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -33,9 +34,9 @@ class ProductController {
 //    }
 
     @GetMapping
-    fun getProducts(): ResponseEntity<List<Product>> {
-        val products = productRepository.getAll()
-        return ResponseEntity.ok(products)
+    fun getProducts(model: Model): String {
+        model.addAttribute("products", productRepository.getAll())
+        return "table"
     }
 
     @GetMapping("/{id}")
