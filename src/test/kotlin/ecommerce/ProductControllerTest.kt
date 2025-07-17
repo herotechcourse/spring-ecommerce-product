695b115ca050c1ae8f55f8dce65fbc7a7ad006e3
pebencze <petra.bencze@deliveryhero.com>
2025-07-17 17:58:48 +0200

    refactor(ProductControllerTest): refactor tests according to the new database implementation

--- src/test/kotlin/ecommerce/ProductControllerTest.kt
+++ src/test/kotlin/ecommerce/ProductControllerTest.kt
@@ -1,18 +1,44 @@
 package ecommerce
 
-import com.fasterxml.jackson.core.type.TypeReference
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.restassured.RestAssured
 import io.restassured.http.ContentType
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpStatus
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.annotation.DirtiesContext
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class ProductControllerTest {
+    @Autowired
+    private lateinit var productRepository: ProductRepository
+
+    @Autowired
+    private lateinit var jdbcTemplate: JdbcTemplate
+
+    @BeforeEach
+    fun setUp() {
+        productRepository = ProductRepository(jdbcTemplate)
+
+        jdbcTemplate.execute("DROP TABLE products IF EXISTS")
+        jdbcTemplate.execute(
+            "CREATE TABLE products(" + "id SERIAL, name VARCHAR(100), price DECIMAL(10,2), image_url VARCHAR(500))",
+        )
+
+        val splitUpAttributes: List<Array<String>> =
+            listOf(
+                "cola 2 http//cola",
+                "fanta 3 http//fanta",
+                "coffee 4 http//coffee",
+                "tea 4 http//tea",
+            ).map { name -> name.split(" ").toTypedArray() }.toList()
+        jdbcTemplate.batchUpdate("INSERT INTO products(name, price, image_url) VALUES (?,?,?)", splitUpAttributes)
+    }
+
     @Test
     fun create() {
         val response =
@@ -24,25 +50,12 @@ class ProductControllerTest {
 
     @Test
     fun read() {
-        create()
-
-        val response =
-            RestAssured.given().log().all().contentType(ContentType.JSON).`when`().get("/products").then().log().all()
-                .extract()
-
-        val rawMap = response.jsonPath().getMap<String, Any>("")
-        val mapper = jacksonObjectMapper()
-        val typeRef = object : TypeReference<Map<Long, Product>>() {}
-        val productMap = mapper.convertValue(rawMap, typeRef)
-
-        assertThat(productMap).hasSize(1)
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value())
+        val products = productRepository.findAllProducts()
+        assertThat(products.size).isEqualTo(4)
     }
 
     @Test
     fun `update existing product`() {
-        create()
-
         val response =
             RestAssured.given().log().all().body(Product(name = "fanta", price = 5.6, imageUrl = "https://fanta.jpg"))
                 .contentType(ContentType.JSON).`when`().put("/products/1").then().log().all().extract()
@@ -51,32 +64,8 @@ class ProductControllerTest {
     }
 
     @Test
-    fun `update non-existing product, test if new product was created`() {
-        val responseAfterPut =
-            RestAssured.given().log().all().body(Product(name = "fanta", price = 5.6, imageUrl = "https://fanta.jpg"))
-                .contentType(ContentType.JSON).`when`().put("/products/3").then().log().all().extract()
-
-        assertThat(responseAfterPut.statusCode()).isEqualTo(HttpStatus.OK.value())
-
-        val responseAfterGet =
-            RestAssured.given().log().all().contentType(ContentType.JSON).`when`().get("/products").then().log().all()
-                .extract()
-
-        val rawMap = responseAfterGet.jsonPath().getMap<String, Any>("")
-        val mapper = jacksonObjectMapper()
-        val typeRef = object : TypeReference<Map<Long, Product>>() {}
-        val productMap = mapper.convertValue(rawMap, typeRef)
-
-        assertThat(productMap).hasSize(1)
-        assertThat(responseAfterGet.statusCode()).isEqualTo(HttpStatus.OK.value())
-    }
-
-    @Test
     fun delete() {
-        create()
-
         val response = RestAssured.given().log().all().`when`().delete("/products/1").then().log().all().extract()
-
         assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value())
     }
 }
