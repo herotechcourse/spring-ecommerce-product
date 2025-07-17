9eb1c69dbada14a4fc1e3601a6f0f7445a4637af
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-17 15:57:52 +0200

    refactor: remove quotes

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO PRODUCTS (name, price, image_url)
+VALUES
+    ('Coffee', 10.99, 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Roasted_coffee_beans.jpg/1200px-Roasted_coffee_beans.jpg'),
+    ('Hand cream', 6.99, 'https://cdn.idealo.com/folder/Product/6178/3/6178302/s1_produktbild_gross/neutrogena-hydro-boost-creme-gel-50ml.jpg'),
+    ('Chair', 77.99, 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRqUHblIQ5QCehPdDXFHqSqUKqxNExCjw_mKe-wsYJYNXP7S7pnlrGpBKEPGzutl3CSIxRQSJjb3HbbQevqNeDMZQdxtP0ml591k0zsn-b-4KAMs9aMwJngvHNPGf7KHbNhUuOn6g&usqp=CAc'),
+    ('Notebook', 3.49, 'https://upload.wikimedia.org/wikipedia/commons/6/6e/Moleskine_notebook.jpg'),
+    ('Bluetooth Speaker', 25.00, 'https://upload.wikimedia.org/wikipedia/commons/d/dd/Bluetooth_speaker_01.jpg'),
+    ('Water Bottle', 12.49, 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Water_bottle.png'),
+    ('Backpack', 39.95, 'https://upload.wikimedia.org/wikipedia/commons/5/57/Backpack_black.jpg'),
+    ('Sneakers', 59.99, 'https://upload.wikimedia.org/wikipedia/commons/e/e8/Sneakers_black.jpg'),
+    ('Desk Lamp', 18.25, 'https://upload.wikimedia.org/wikipedia/commons/f/f4/LED_desk_lamp.jpg'),
+    ('Sunglasses', 22.00, 'https://upload.wikimedia.org/wikipedia/commons/8/83/Sunglasses_on_white_background.jpg');
