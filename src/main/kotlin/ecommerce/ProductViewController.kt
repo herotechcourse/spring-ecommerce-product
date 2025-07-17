94405af679759434776c13866f7f8b29a623823f
Sanghun Park <po4865@gmail.com>
2025-07-17 17:30:47 +0200

    refactor(ProductViewController,Views): edit views to fetch from api directly

--- src/main/kotlin/ecommerce/ProductViewController.kt
+++ src/main/kotlin/ecommerce/ProductViewController.kt
@@ -1,21 +1,13 @@
 package ecommerce
 
-import ecommerce.api.ProductController
 import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
-class ProductViewController(
-    private val controller: ProductController,
-) {
+class ProductViewController {
     @GetMapping("/")
-    fun displayProducts(model: Model): String {
-        // TODO: Figure out how to deal with controller connection to get data
-        val products = controller.products.values.toList()
-
-        model.addAttribute("products", products)
+    fun displayProducts(): String {
         return "products"
     }
 
@@ -27,10 +19,7 @@ class ProductViewController(
     @GetMapping("/update/product/{id}")
     fun displayUpdateProductForm(
         @PathVariable id: Long,
-        model: Model,
     ): String {
-        val product = controller.products[id]
-        model.addAttribute("product", product)
         return "updateProductForm"
     }
 }
