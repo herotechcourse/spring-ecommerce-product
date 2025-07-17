7b17e5a176f85856032cd06271f93e9fcc7d26e7
pebencze <petra.bencze@deliveryhero.com>
2025-07-17 17:06:59 +0200

    feat(resources/*): initialize database with data.sql and schema.sql files, add dependencies to application.properties

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO products (name,price,image_url) VALUES ('Cola', 2, 'https://t4.ftcdn.net/jpg/02/84/65/61/360_F_284656117_sPF8gVWaX627bq5qKrlrvCz1eFfowdBf.jpg');
+INSERT INTO products (name,price,image_url) VALUES ('Fanta', 2, 'https://media.istockphoto.com/id/493952763/photo/can-of-fanta-orange.jpg?s=612x612&w=0&k=20&c=NcwCzRXwfB5jghmlAMDW-I26pNVa6UKtwuCfeJk5YEo=');
+INSERT INTO products (name,price,image_url) VALUES ('Coffee', 2, 'https://www.foodandwine.com/thmb/CCe2JUHfjCQ44L0YTbCu97ukUzA=/1500x0/filters:no_upscal[…]ners-Latte-FT-BLOG0523-09569880de524fe487831d95184495cc.jpg');
