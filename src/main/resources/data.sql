fcca3b555067bbee480cbd52412067d7cba5d3d4
Sanghun Park <po4865@gmail.com>
2025-07-17 15:35:40 +0200

    feat: add H2 in-memory database configuration with schema and data scripts

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO product (name, price, imageUrl) VALUES ('Iron Man', 1000, 'https://alexnsan.comics/imageurl/1');
+INSERT INTO product (name, price, imageUrl) VALUES ('X-men', 1000, 'https://alexnsan.comics/imageurl/2');
+INSERT INTO product (name, price, imageUrl) VALUES ('Superman', 1000, 'https://alexnsan.comics/imageurl/3');
+INSERT INTO product (name, price, imageUrl) VALUES ('Naruto', 1000, 'https://alexnsan.comics/imageurl/4');
+INSERT INTO product (name, price, imageUrl) VALUES ('Full Metal Alchemist', 1000, 'https://alexnsan.comics/imageurl/5');
