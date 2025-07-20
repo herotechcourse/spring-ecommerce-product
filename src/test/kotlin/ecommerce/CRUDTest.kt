0583019ea971f9a4d0209477835312433ebce88a
Deniz <69081375+deniz-oezdemir@users.noreply.github.com>
2025-07-20 11:19:41 +0200

    [spring-ecommerce-product - Step 1-1 to 1-3] Submit Deniz' mission (#1)
    * feat(ProductController, Product): add support for post request
    - Add test for post response
    
    * feat(ProductController, Product): add support for get request
    - Add test for get response
    
    * feat(ProductController, Product): add support for put request
    - Add test for put response
    
    * feat(ProductController, Product): add support for delete request
    - Add test for delete response
    
    * test(ProductController, CRUDTest): add tests for failing update and delete requests
    - Add exception handler for RuntimeExceptions
    
    * test(CRUDTest): fix tests for failing update and delete requests
    
    * test(products.html): add delete product request functionality to website
    
    * feat(products.html): add update product form to website
    
    * feat(database integration): replace global properties with H2 database (DB)
    - Add application properties for DB
    - Add DB schema
    - Add ProductRepository class as middleware between DB and application
    - Refactor ProductController to use DB
    - Refactor CRUD and SSR tests
    - Add DB tests
    
    * style(): format source files with ktlint
    
    * refactor(): incorporate feedback from review
    - Solve test interference with port setting and database existence checks
    - Add checks for name and imageUrl length in `Product` initialization and tests
    - Add override functions for equals and hashCode for to future-proof `Product`
    - Add `RequestMapping` annotation
    - Replace `Controller` annotation with `RestController` annotation for `ProductController` class
    - Remove unnecessary `DirtiesContext` annotation

---
+++ src/test/kotlin/ecommerce/CRUDTest.kt
@@ -0,0 +1,168 @@
+package ecommerce
+
+import io.restassured.RestAssured
+import io.restassured.http.ContentType
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.http.HttpStatus
+import org.springframework.jdbc.core.JdbcTemplate
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CRUDTest {
+    private lateinit var productRepository: ProductRepository
+
+    @LocalServerPort
+    private var port: Int = 0
+
+    @Autowired
+    private lateinit var jdbcTemplate: JdbcTemplate
+
+    @BeforeEach
+    fun setUp() {
+        productRepository = ProductRepository(jdbcTemplate)
+
+        jdbcTemplate.execute("DROP TABLE products IF EXISTS")
+        jdbcTemplate.execute(
+            "CREATE TABLE IF NOT EXISTS products(" +
+                    "id BIGINT AUTO_INCREMENT, name VARCHAR(255) NOT NULL, price DOUBLE NOT NULL, image_url VARCHAR(512) NOT NULL)",
+        )
+
+        val products =
+            listOf(
+                Product(
+                    id = 1L,
+                    name = "vanilla ice cream",
+                    price = 1.99,
+                    imageUrl = "https://laurenslatest.com/wp-content/uploads/2020/08/vanilla-ice-cream-5-copy-360x361.jpg",
+                ),
+                Product(
+                    id = 2L,
+                    name = "pistachio ice cream",
+                    price = 2.49,
+                    imageUrl = "https://greenhealthycooking.com/wp-content/uploads/2017/06/Pistachio-Ice-Cream-Photo.jpg",
+                ),
+                Product(
+                    id = 3L,
+                    name = "chocolate ice cream",
+                    price = 1.49,
+                    imageUrl = "https://www.cravethegood.com/wp-content/uploads/2021/04/sous-vide-chocolate-ice-cream-15.jpg",
+                ),
+            )
+
+        jdbcTemplate.batchUpdate(
+            "INSERT INTO products(name, price, image_url) VALUES (?, ?, ?)",
+            products,
+            products.size,
+        ) { ps, product ->
+            ps.setString(1, product.name)
+            ps.setDouble(2, product.price)
+            ps.setString(3, product.imageUrl)
+        }
+    }
+
+    @Test
+    fun create() {
+        val response =
+            RestAssured
+                .given().log().all()
+                .port(port)
+                .body(
+                    Product(
+                        name = "Orange ice cream",
+                        price = 2.80,
+                        imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf8Fdb-33SCOszWX_UF-92pCwX4Rcam0uVCg&s",
+                    ),
+                )
+                .contentType(ContentType.JSON)
+                .`when`().post("/api/products")
+                .then().log().all().extract()
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value())
+    }
+
+    @Test
+    fun read() {
+        val response =
+            RestAssured
+                .given().log().all()
+                .port(port)
+                .contentType(ContentType.JSON)
+                .`when`().get("/api/products")
+                .then().log().all().extract()
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value())
+        assertThat(response.jsonPath().getList("", Product::class.java)).hasSize(3)
+    }
+
+    @Test
+    fun update_success() {
+        val response =
+            RestAssured
+                .given().log().all()
+                .port(port)
+                .body(
+                    Product(
+                        name = "lemon ice cream",
+                        price = 3.60,
+                        imageUrl =
+                            "https://www.carnation.co.uk/sites/default/files/2020" +
+                                    "-05/Final%20Lemon%20Curd%20Ice%20Cream%20mobile.jpg",
+                    ),
+                )
+                .contentType(ContentType.JSON)
+                .`when`().put("/api/products/1")
+                .then().log().all().extract()
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value())
+    }
+
+    @Test
+    fun update_failure() {
+        val response =
+            RestAssured
+                .given().log().all()
+                .port(port)
+                .body(
+                    Product(
+                        name = "vanilla ice cream",
+                        price = 3.60,
+                        imageUrl = "https://laurenslatest.com/wp-content/uploads/2020/08/vanilla-ice-cream-5-copy-360x361.jpg",
+                    ),
+                )
+                .contentType(ContentType.JSON)
+                .`when`().put("/api/products/4")
+                .then().log().all().extract()
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value())
+    }
+
+    @Test
+    fun delete_success() {
+        create()
+
+        val response =
+            RestAssured
+                .given().log().all()
+                .port(port)
+                .`when`().delete("/api/products/1")
+                .then().log().all().extract()
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value())
+    }
+
+    @Test
+    fun delete_failure() {
+        val response =
+            RestAssured
+                .given().log().all()
+                .port(port)
+                .`when`().delete("/api/products/4")
+                .then().log().all().extract()
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value())
+    }
+}
