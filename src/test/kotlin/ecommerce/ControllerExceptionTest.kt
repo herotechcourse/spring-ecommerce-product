d0b6ceb8537ac12f27ac5105646d80fb6abc6cae
pebencze <petra.bencze@deliveryhero.com>
2025-07-21 16:40:33 +0200

    feat(test): create TDD for exception handler; test validation logic

---
+++ src/test/kotlin/ecommerce/ControllerExceptionTest.kt
@@ -0,0 +1,47 @@
+package ecommerce
+
+import io.restassured.RestAssured
+import io.restassured.http.ContentType
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpStatus
+import org.springframework.test.annotation.DirtiesContext
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class ControllerExceptionTest {
+    @Test
+    fun handleExceptionUsingExceptionHandler() {
+        val response =
+            RestAssured
+                .given().log().all().body(Product(name = "colaaaaaaaaaaaaaaaaaaaaaaaaaaaa", price = 4.5, imageUrl = "https://cola.jpg"))
+                .contentType(ContentType.JSON)
+                .`when`().post("/products")
+                .then().log().all().extract()
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value())
+        assertThat(response.headers().toString()).contains("Product name cannot exceed 15 characters")
+    }
+
+    @Test
+    fun sameNameException() {
+        val response =
+            RestAssured
+                .given().log().all().body(Product(name = "cola", price = 4.5, imageUrl = "https://cola.jpg"))
+                .contentType(ContentType.JSON)
+                .`when`().post("/products")
+                .then().log().all().extract()
+
+        val sameNameResponse =
+            RestAssured
+                .given().log().all().body(Product(name = "cola", price = 4.5, imageUrl = "https://cola.jpg"))
+                .contentType(ContentType.JSON)
+                .`when`().post("/products")
+                .then().log().all().extract()
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value())
+        assertThat(sameNameResponse.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value())
+        assertThat(sameNameResponse.headers().toString()).contains("Product with name 'cola' already exists")
+    }
+}
