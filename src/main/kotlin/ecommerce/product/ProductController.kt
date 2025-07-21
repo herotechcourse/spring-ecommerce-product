351f3dae6f0109667a2c1e93bd150075a80ccf69
knopgm <knop.gm@gmail.com>
2025-07-21 11:19:37 +0200

    refactor (ProductController): replace @Controller with @RestController to make clear it is returning a JSON and add @RequestMapping to set the base path to all endpoint calls

--- src/main/kotlin/ecommerce/product/ProductController.kt
+++ src/main/kotlin/ecommerce/product/ProductController.kt
@@ -2,21 +2,23 @@ package ecommerce.product
 
 import ecommerce.repository.ProductRepository
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import java.net.URI
 import java.util.concurrent.atomic.AtomicLong
 
-@Controller
+@RequestMapping("/api/products")
+@RestController
 class ProductController(private val repository: ProductRepository) {
     private val index = AtomicLong(1)
 
-    @PostMapping("/api/products")
+    @PostMapping("")
     fun create(
         @RequestBody product: Product,
     ): ResponseEntity<Product> {
@@ -27,13 +29,13 @@ class ProductController(private val repository: ProductRepository) {
         return ResponseEntity.created(URI.create("/products/" + newProduct.id)).body(product)
     }
 
-    @GetMapping("/api/products")
+    @GetMapping("")
     fun read(): ResponseEntity<List<Product>> {
         if (repository.isEmptyOrNull()) return ResponseEntity.noContent().build()
         return ResponseEntity.ok(repository.findAll())
     }
 
-    @PutMapping("/api/products/{id}")
+    @PutMapping("/{id}")
     fun update(
         @RequestBody newProduct: Product,
         @PathVariable id: Long,
@@ -44,7 +46,7 @@ class ProductController(private val repository: ProductRepository) {
         return ResponseEntity.ok().body(product)
     }
 
-    @DeleteMapping("/api/products/{id}")
+    @DeleteMapping("/{id}")
     fun delete(
         @PathVariable id: Long,
     ): ResponseEntity<Void> {
