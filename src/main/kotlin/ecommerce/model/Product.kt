defc5430eb18a74b1bc7fe359acc38d917a52947
farhanaahmed <farhana.ahmed.fima@gmail.com>
2025-07-21 15:30:29 +0200

    refactor: implement equality based on id in Product class

--- src/main/kotlin/ecommerce/model/Product.kt
+++ src/main/kotlin/ecommerce/model/Product.kt
@@ -4,20 +4,15 @@ class Product(
     val id: Long? = null,
     var name: String = "",
     var price: Double = 0.0,
-    var imageUrl: String = "",
+    var imageUrl: String = ""
 ) {
-    fun update(newProduct: Product) {
-        this.name = newProduct.name
-        this.price = newProduct.price
-        this.imageUrl = newProduct.imageUrl
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Product) return false
+        return id == other.id
     }
 
-    companion object {
-        fun toEntity(
-            product: Product,
-            id: Long,
-        ): Product {
-            return Product(id, product.name, product.price, product.imageUrl)
-        }
+    override fun hashCode(): Int {
+        return id?.hashCode() ?: 0
     }
 }
