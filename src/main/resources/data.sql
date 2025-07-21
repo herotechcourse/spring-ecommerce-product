53f881b7ed5073623d67b871ba74b11fde19aa8e
Jin Park <jieun.park.e@deliveryhero.com>
2025-07-21 18:06:51 +0200

    style: change table's name to lowercase

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO TBL_PRODUCTS (name, price, imageUrl) VALUES
+INSERT INTO products (name, price, imageUrl) VALUES
 ('Car', 1000.0, 'https://images.unsplash.com/photo-1494905998402-395d579af36f?w=400&h=400&fit=crop'),
 ('Bike', 200.0, 'https://images.unsplash.com/photo-1571068316344-75bc76f77890?w=400&h=400&fit=crop'),
 ('Truck', 30000.0, 'https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=400&h=400&fit=crop'),
