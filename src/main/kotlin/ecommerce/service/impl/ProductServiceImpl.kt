997b13a337d25031459cc4090ad5a254ee8c58b8
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-21 19:11:57 +0200

    feat: add first implementation for service

---
+++ src/main/kotlin/ecommerce/service/impl/ProductServiceImpl.kt
@@ -0,0 +1,35 @@
+package ecommerce.service.impl
+
+import ecommerce.dto.ProductRequest
+import ecommerce.dto.ProductResponse
+import ecommerce.model.toDto
+import ecommerce.repository.ProductRepository
+import ecommerce.service.ProductService
+import org.springframework.stereotype.Service
+
+@Service
+class ProductServiceImpl(private val productRepository: ProductRepository) : ProductService {
+    override fun findById(id: Long): ProductResponse? {
+        val product = productRepository.findById(id) ?: throw NoSuchElementException("Product not found, id: $id")
+        return product.toDto()
+    }
+
+    override fun findAll(): List<ProductResponse> {
+        TODO("Not yet implemented")
+    }
+
+    override fun createProduct(productRequest: ProductRequest): ProductResponse {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateProduct(
+        id: Long,
+        productRequest: ProductRequest,
+    ): ProductResponse? {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteProduct(id: Long): Boolean {
+        TODO("Not yet implemented")
+    }
+}
