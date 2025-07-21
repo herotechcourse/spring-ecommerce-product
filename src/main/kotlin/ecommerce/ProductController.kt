7ad52cff5ab54daa79e2b64ee8f35a9833d228a9
pebencze <petra.bencze@deliveryhero.com>
2025-07-21 11:49:09 +0200

    refactor(ProductController, products.html): rep;lace PUT by PATCH for update

--- src/main/kotlin/ecommerce/ProductController.kt
+++ src/main/kotlin/ecommerce/ProductController.kt
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
@@ -30,7 +31,7 @@ class ProductController(private val productRepository: ProductRepository) {
         return ResponseEntity.ok().body(products)
     }
 
-    @PutMapping("/products/{id}")
+    @PatchMapping("/products/{id}")
     @ResponseBody
     fun update(
         @RequestBody newProduct: Product,
