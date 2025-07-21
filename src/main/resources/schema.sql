1177719e45bce13268aa5a86cdfa3660de43986b
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-07-21 13:30:50 +0200

    refactor: Use same type for db and app

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS products (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
     product_name VARCHAR(255) NOT NULL,
     price DOUBLE CHECK (price >= 0),
     image_url VARCHAR(255)
-    );
+);
