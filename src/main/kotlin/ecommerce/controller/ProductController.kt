0d455755eed413407b98d2e62a8d5f6e20e4f16f
Jin Park <jieun.park.e@deliveryhero.com>
2025-07-21 18:14:21 +0200

    remove: unnecessary requestMapping-related lines

--- src/main/kotlin/ecommerce/controller/ProductController.kt
+++ src/main/kotlin/ecommerce/controller/ProductController.kt
@@ -10,12 +10,10 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.net.URI
 
 @RestController
-@RequestMapping
 class ProductController(private val productRepository: ProductRepository) {
     @GetMapping(PRODUCT_PATH)
     fun getProducts(): List<Product> = productRepository.findAll()
