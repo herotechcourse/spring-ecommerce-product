a963918afdb146ba2a90bca902ee3cc91c606104
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-07-21 14:44:31 +0200

    lint: formating

--- src/test/kotlin/ecommerce/controller/ProductControllerTest.kt
+++ src/test/kotlin/ecommerce/controller/ProductControllerTest.kt
@@ -10,10 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpStatus
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.support.GeneratedKeyHolder
-import org.springframework.test.annotation.DirtiesContext
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ProductControllerTest {
     @Autowired
     private lateinit var jdbcTemplate: JdbcTemplate
