e0357a7ee5618a57f24cadb86c2cd62be2d0dfa6
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-07-21 10:04:05 +0200

    refactor: remove unused code

--- src/main/kotlin/ecommerce/model/Product.kt
+++ src/main/kotlin/ecommerce/model/Product.kt
@@ -1,23 +1,8 @@
 package ecommerce.model
 
-class Product(
+data class Product(
     val id: Long? = null,
     var name: String = "",
     var price: Double = 0.0,
     var imageUrl: String = "",
-) {
-    fun update(newProduct: Product) {
-        this.name = newProduct.name
-        this.price = newProduct.price
-        this.imageUrl = newProduct.imageUrl
-    }
-
-    companion object {
-        fun toEntity(
-            product: Product,
-            id: Long,
-        ): Product {
-            return Product(id, product.name, product.price, product.imageUrl)
-        }
-    }
-}
+)
