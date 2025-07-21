7df1664c067a6d2d86045c365974d22966ed9451
knopgm <knop.gm@gmail.com>
2025-07-21 11:01:57 +0200

    feat (Product): override equals() and hashCode() in product class so that comparisons (like in sets, maps, or during testing) it rely only on id

--- src/main/kotlin/ecommerce/product/Product.kt
+++ src/main/kotlin/ecommerce/product/Product.kt
@@ -7,6 +7,18 @@ class Product(var id: Long? = null, var name: String, var price: Double, var ima
         this.imageUrl = newProduct.imageUrl
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Product
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id?.hashCode() ?: 0
+    }
+
     companion object {
         fun toEntity(
             product: Product,
