3250117c800ef8e1f1c2f0eac151537307d33801
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-07-21 11:12:31 +0200

    refactor: use beans to initialize jdbc template

--- src/main/kotlin/ecommerce/controller/ProductController.kt
+++ src/main/kotlin/ecommerce/controller/ProductController.kt
@@ -15,24 +15,24 @@ import java.net.URI
 
 @RestController
 @RequestMapping("/products")
-class ProductController {
+class ProductController(private val productRepository: ProductRepository) {
     @GetMapping
     fun getProducts(): ResponseEntity<List<Product>> {
-        return ResponseEntity.ok().body(ProductRepository().findAll())
+        return ResponseEntity.ok().body(productRepository.findAll())
     }
 
     @GetMapping("/{id}")
     fun getProductById(
         @PathVariable("id") id: Long,
     ): ResponseEntity<Product> {
-        return ResponseEntity.ok().body(ProductRepository().findById(id))
+        return ResponseEntity.ok().body(productRepository.findById(id))
     }
 
     @PostMapping("")
     fun create(
         @RequestBody product: Product,
     ): ResponseEntity<Void> {
-        ProductRepository().save(product)
+        productRepository.save(product)
         return ResponseEntity.created(URI.create("/products/")).build()
     }
 
@@ -41,7 +41,7 @@ class ProductController {
         @PathVariable("id") id: Long,
         @RequestBody newProduct: Product,
     ): ResponseEntity<Void> {
-        ProductRepository().update(id, newProduct)
+        productRepository.update(id, newProduct)
         return ResponseEntity.ok().build()
     }
 
@@ -49,7 +49,7 @@ class ProductController {
     fun delete(
         @PathVariable("id") id: Long,
     ): ResponseEntity<Void> {
-        ProductRepository().deleteById(id)
+        productRepository.deleteById(id)
         return ResponseEntity.noContent().build()
     }
 }
--- src/main/kotlin/ecommerce/repository/ProductRepository.kt
+++ src/main/kotlin/ecommerce/repository/ProductRepository.kt
@@ -1,17 +1,13 @@
 package ecommerce.repository
 
 import ecommerce.model.Product
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.core.RowMapper
 import org.springframework.stereotype.Repository
 import java.sql.ResultSet
 
 @Repository
-class ProductRepository() {
-    @Autowired
-    private lateinit var jdbcTemplate: JdbcTemplate
-
+class ProductRepository(private val jdbcTemplate: JdbcTemplate) {
     private val productRowMapper =
         RowMapper<Product> { rs: ResultSet, _ ->
             Product(
