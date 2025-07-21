459d6846d9a54efe1879436afdcde8ee2833f809
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-21 11:42:09 +0200

    refactor: adapt tests to new end points

--- src/test/kotlin/ecommerce/controller/ProductControllerTest.kt
+++ src/test/kotlin/ecommerce/controller/ProductControllerTest.kt
@@ -1,39 +1,53 @@
 package ecommerce.controller
 
 import ecommerce.model.Product
+import ecommerce.repository.ProductRepository
 import io.restassured.RestAssured
 import io.restassured.http.ContentType
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpStatus
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.annotation.DirtiesContext
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
+    properties = ["spring.sql.init.mode=never"],
+)
 class ProductControllerTest {
-    private lateinit var products: Set<Product>
+    private lateinit var productRepository: ProductRepository
+
+    @Autowired
+    private lateinit var jdbcTemplate: JdbcTemplate
 
     @BeforeEach
-    fun createProducts() {
+    fun setUp() {
+        productRepository = ProductRepository(jdbcTemplate)
+
+        jdbcTemplate.execute("DROP TABLE products IF EXISTS")
+
+        jdbcTemplate.execute(createQuery())
+
         val product1 = Product(1L, "cafe", 39.00, "www.test")
         val product2 = Product(2L, "table", 39.00, "www.test")
         val product3 = Product(3L, "chair", 39.00, "www.test")
-        val productsExpected =
-            RestAssured
-                .given().log().all().body(product1)
-                .contentType(ContentType.JSON)
-                .`when`()
-                .request("POST", "/products")
-                .then()
-                .extract()
-                .response()
+        RestAssured
+            .given().log().all().body(product1)
+            .contentType(ContentType.JSON)
+            .`when`()
+            .request("POST", "/api/products")
+            .then()
+            .extract()
+            .response()
         RestAssured
             .given().log().all().body(product2)
             .contentType(ContentType.JSON)
             .`when`()
-            .request("POST", "/products")
+            .request("POST", "/api/products")
             .then()
             .extract()
             .response()
@@ -41,30 +55,49 @@ class ProductControllerTest {
             .given().log().all().body(product3)
             .contentType(ContentType.JSON)
             .`when`()
-            .request("POST", "/products")
+            .request("POST", "/api/products")
             .then()
             .extract()
             .response()
-        products = mutableSetOf(product1, product2, product3)
+    }
+
+    private fun createQuery(): String {
+        return """
+            create table PRODUCTS
+            (
+                ID       int              not null AUTO_INCREMENT,
+                NAME     varchar(100)     not null,
+                PRICE    double not null,
+                IMAGE_URL varchar(500),
+                PRIMARY KEY (ID)
+            )
+            """.trimIndent()
     }
 
     @Test
     fun addProduct() {
-        val product = Product(1L, "cafe", 39.00, "www.test")
         val response =
             RestAssured
-                .given().log().all().body(product)
+                .given().log().all().body("""{ "name": "Screen", "price": 190.0, "imageUrl": "ciao" }""")
                 .contentType(ContentType.JSON)
                 .`when`()
-                .request("POST", "/products")
+                .request("POST", "/api/products")
                 .then()
                 .extract()
                 .response()
 
-        assertThat(response.statusCode).isEqualTo(HttpStatus.OK.value())
-        assertThat(response.`as`(Product::class.java).name).isEqualTo("cafe")
-        assertThat(response.`as`(Product::class.java).price).isEqualTo(39.00)
-        assertThat(response.`as`(Product::class.java).imageUrl).isEqualTo("www.test")
+        val newProduct =
+            RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .`when`()
+                .request("GET", "/api/products/4")
+                .then()
+                .extract()
+                .response()
+
+        assertThat(response.statusCode).isEqualTo(HttpStatus.CREATED.value())
+        assertThat(newProduct.`as`(Product::class.java).name).isEqualTo("Screen")
     }
 
     @Test
@@ -79,7 +112,6 @@ class ProductControllerTest {
                 .response()
 
         assertThat(response.statusCode).isEqualTo(HttpStatus.OK.value())
-        assertThat(response.body.jsonPath().getList<Product>("")).hasSize(3)
     }
 
     @Test
@@ -89,7 +121,7 @@ class ProductControllerTest {
             RestAssured
                 .given().log().all()
                 .`when`()
-                .request("GET", "/products/1")
+                .request("GET", "/api/products/1")
                 .then()
                 .extract()
                 .response()
