af3c1a1959451e3d91d9b4748a67e419c100a872
farhanaahmed <farhana.ahmed.fima@gmail.com>
2025-07-21 10:54:17 +0200

    chore: add data.sql with initial product records

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO products(product_name, price, image_url)
+VALUES ('Product 1', 10.2, 'url.com');
+INSERT INTO products(product_name, price, image_url)
+VALUES ('Phone', 5000.0, 'phone.com');
+INSERT INTO products(product_name, price, image_url)
+VALUES ('Laptop', 15000.0, 'laptop.com');
