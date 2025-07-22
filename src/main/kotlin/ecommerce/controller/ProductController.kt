b68c43bed9d319bbd95fa61283fed12a39079dc5
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-07-22 12:59:05 +0200

    refactor: use service in the controller

--- src/main/kotlin/ecommerce/controller/ProductController.kt
+++ src/main/kotlin/ecommerce/controller/ProductController.kt
@@ -1,57 +1,48 @@
 package ecommerce.controller
 
-import ecommerce.model.Product
-import ecommerce.repository.ProductRepository
+import ecommerce.dto.ProductDTO
+import ecommerce.service.ProductService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.net.URI
 
 @RestController
 @RequestMapping("/products")
-class ProductController(private val productRepository: ProductRepository) {
+class ProductController(private val productService: ProductService) {
     @GetMapping
-    fun getProducts(): ResponseEntity<List<Product>> {
-        return ResponseEntity.ok().body(productRepository.findAll())
-    }
+    fun getProducts(): ResponseEntity<List<ProductDTO>> = productService.getAllProducts()
 
     @GetMapping("/{id}")
     fun getProductById(
         @PathVariable("id") id: Long,
-    ): ResponseEntity<Product> {
-        val product =
-            productRepository.findById(id)
-        return ResponseEntity.ok().body(product)
-    }
+    ): ResponseEntity<ProductDTO> = productService.getProductById(id)
 
     @PostMapping("")
     fun create(
-        @RequestBody product: Product,
-    ): ResponseEntity<Void> {
-        productRepository.save(product)
-        return ResponseEntity.created(URI.create("/products/")).build()
-    }
+        @RequestBody product: ProductDTO,
+    ): ResponseEntity<Void> = productService.createProduct(product)
 
     @PutMapping("/{id}")
     fun update(
         @PathVariable("id") id: Long,
-        @RequestBody newProduct: Product,
-    ): ResponseEntity<Void> {
-        productRepository.update(id, newProduct)
-        return ResponseEntity.ok().build()
-    }
+        @RequestBody newProduct: ProductDTO,
+    ): ResponseEntity<Void> = productService.updateProduct(id, newProduct)
+
+    @PatchMapping("/{id}")
+    fun edit(
+        @PathVariable("id") id: Long,
+        @RequestBody newProduct: ProductDTO,
+    ): ResponseEntity<Void> = productService.updateProduct(id, newProduct)
 
     @DeleteMapping("/{id}")
     fun delete(
         @PathVariable("id") id: Long,
-    ): ResponseEntity<Void> {
-        productRepository.deleteById(id)
-        return ResponseEntity.noContent().build()
-    }
+    ): ResponseEntity<Void> = productService.deleteProduct(id)
 }
