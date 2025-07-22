7c5489a08101ed611bdffa6919b696463ca5dea9
arekiu <alexis.aguilera@deliveryhero.com>
2025-07-22 18:39:05 +0200

    feat(GlobalExceptionHandler): add exception handler class

---
+++ src/main/kotlin/ecommerce/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,18 @@
+package ecommerce.exception
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleValidationException(ex: MethodArgumentNotValidException): ResponseEntity<Map<String, Any>> {
+        val errors = ex.bindingResult.fieldErrors.associate {
+            it.field to (it.defaultMessage ?: "Invalid value")
+        }
+        return ResponseEntity.badRequest().body(mapOf("errors" to errors))
+    }
+}
