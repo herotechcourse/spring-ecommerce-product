16e66ba93c4247a3f88213f3908885324a680e29
pebencze <petra.bencze@deliveryhero.com>
2025-07-22 15:57:20 +0200

    refactor(ProductViewController): extract view management to another controller

---
+++ src/main/kotlin/ecommerce/ProductViewController.kt
@@ -0,0 +1,15 @@
+package ecommerce
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class ProductViewController(private val productRepository: ProductRepository) {
+    @GetMapping("/admin/products")
+    fun table(model: Model): String {
+        model.addAttribute("products", productRepository.findAllProducts())
+        model.addAttribute("product", Product())
+        return "products"
+    }
+}
