4ce12b77a43537801f89b4c9d6eea6eb3f2986f5
AzureAD\AnastasiiaLvutina <anasteishanakir@gmail.com>
2025-07-22 15:06:00 +0200

    feat(TokenRequest): create data class `TokenRequest` - data class has ``email: String`` - data class has ``password: String``

---
+++ src/main/kotlin/ecommerce/TokenRequest.kt
@@ -0,0 +1,12 @@
+package ecommerce
+
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
+data class TokenRequest(@field:NotBlank(message = "Email cannot be blank")
+                        @field:Email(message = "Email must be a valid email address")
+                        val email: String,
+                        @field:NotBlank(message = "Password cannot be blank")
+                        @field:Size(min = 8, message = "Password must be at least 8 characters long")
+                        val password: String)
