d4e960352fba475023054af4f633599fe561f979
AzureAD\AnastasiiaLvutina <anasteishanakir@gmail.com>
2025-07-22 14:30:21 +0200

    feat(Member.kt): create new data class ``Member`` - Member has id - Member has email - Member has password - Member has a role

---
+++ src/main/kotlin/ecommerce/Member.kt
@@ -0,0 +1,14 @@
+package ecommerce
+
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
+data class Member(var id: Long? = null,
+                  @field:NotBlank(message = "Email cannot be blank")
+                  @field:Email(message = "Email must be a valid email address")
+                  val email: String,
+                  @field:NotBlank(message = "Password cannot be blank")
+                  @field:Size(min = 8, message = "Password must be at least 8 characters long")
+                  val password: String,
+                  val role: String = "USER")
