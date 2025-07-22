c86f2410e4b9c1d1d5b2f5163de51ac371a24277
Mina Kim <mina.kim@deliveryhero.com>
2025-07-22 17:46:18 +0200

    chore(gradle): add new dependencies for validation "org.springframework.boot:spring-boot-starter-validation"

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,11 +25,12 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("io.rest-assured:rest-assured:5.3.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
