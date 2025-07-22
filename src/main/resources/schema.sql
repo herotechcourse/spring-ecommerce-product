a36332044cda43aeaffa9a9c1959e21a4fcfb764
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-07-22 12:51:20 +0200

    refactor: add attributes in the product table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS products (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    product_name VARCHAR(255) NOT NULL,
+    name VARCHAR NOT NULL UNIQUE,
+    description VARCHAR,
     price DOUBLE CHECK (price >= 0),
-    image_url VARCHAR(255)
+    image_url VARCHAR,
+    quantity INT
 );
