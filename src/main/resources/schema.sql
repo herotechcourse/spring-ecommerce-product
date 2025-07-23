e5dfad796507f50938322776b099d80e4a207807
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-23 12:03:13 +0200

    feat: add members table to schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,15 @@
 create table PRODUCTS
 (
-    ID       int              not null AUTO_INCREMENT,
-    NAME     varchar(100)     not null,
-    PRICE    double not null,
+    ID        int          not null AUTO_INCREMENT,
+    NAME      varchar(100) not null,
+    PRICE double not null,
     IMAGE_URL varchar(500),
     PRIMARY KEY (ID)
+);
+
+CREATE TABLE MEMBERS
+(
+    ID       INT AUTO_INCREMENT PRIMARY KEY,
+    EMAIL    VARCHAR(100) NOT NULL UNIQUE,
+    PASSWORD VARCHAR(100) NOT NULL
 );
