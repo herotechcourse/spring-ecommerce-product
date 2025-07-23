79e71c23b608b099472e99b13eff1ff3f18b4d4b
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-07-23 22:08:11 +0200

    feat: add findMemberCart

--- src/main/kotlin/ecommerce/repository/CartRepository.kt
+++ src/main/kotlin/ecommerce/repository/CartRepository.kt
@@ -1,14 +1,21 @@
 package ecommerce.repository
 
+import ecommerce.mapper.ProductRowMapper
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Repository
 
 @Repository
 class CartRepository(
     private val jdbcTemplate: JdbcTemplate,
+    private val productRowMapper: ProductRowMapper,
 ) {
     fun createCartForUser(userId: Long) {
         val sql = "insert into cart (user_id) values ?"
         jdbcTemplate.update(sql, userId)
+    }
+
+    fun findMembersCart(userId: Long): Long? {
+        val sql = "select * from cart where user_id = ?"
+        return jdbcTemplate.queryForObject(sql, Long::class.javaObjectType, userId)
     }
 }
