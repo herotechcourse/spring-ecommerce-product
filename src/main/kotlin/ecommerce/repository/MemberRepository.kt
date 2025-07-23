6da634944dc11374562ced644b021c5914c2d5c7
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-23 12:01:37 +0200

    feat: add first implementation MemberRepository and save new Member function

---
+++ src/main/kotlin/ecommerce/repository/MemberRepository.kt
@@ -0,0 +1,20 @@
+package ecommerce.repository
+
+import ecommerce.dto.RegistrationRequest
+import org.springframework.jdbc.core.simple.JdbcClient
+import org.springframework.jdbc.support.GeneratedKeyHolder
+import org.springframework.jdbc.support.KeyHolder
+import org.springframework.stereotype.Repository
+
+@Repository
+class MemberRepository(private val db: JdbcClient) {
+    fun save(member: RegistrationRequest): Boolean {
+        val keyHolder: KeyHolder = GeneratedKeyHolder()
+
+        val sql = "INSERT INTO members (email, password) VALUES (?,?)"
+        return db
+            .sql(sql)
+            .params(member.email, member.password)
+            .update(keyHolder) > 0
+    }
+}
