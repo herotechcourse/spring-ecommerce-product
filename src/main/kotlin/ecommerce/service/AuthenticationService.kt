f02a52c4bc2faf9ff1cf3d0ff03d26a47966cef3
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-23 15:54:22 +0200

    refactor: call create token in service if member is saved in db

--- src/main/kotlin/ecommerce/service/AuthenticationService.kt
+++ src/main/kotlin/ecommerce/service/AuthenticationService.kt
@@ -1,17 +1,28 @@
 package ecommerce.service
 
+import ecommerce.configuration.JwtTokenProvider
 import ecommerce.dto.RegistrationRequest
+import ecommerce.dto.TokenResponse
 import ecommerce.exception.MemberEmailAlreadyExistsException
 import ecommerce.repository.MemberRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-class AuthenticationService(private val memberRepository: MemberRepository, repository: MemberRepository) {
-    fun registerMember(request: RegistrationRequest): String {
+class AuthenticationService(
+    private val memberRepository: MemberRepository,
+    private val tokenService: JwtTokenProvider,
+) {
+    @Transactional
+    fun registerMember(request: RegistrationRequest): TokenResponse {
         if (memberRepository.existsByEmail(request.email)) {
             throw MemberEmailAlreadyExistsException("Email already exists: ${request.email}")
         }
-        val created = memberRepository.save(request)
-        return "toDo"
+        val saved = memberRepository.save(request)
+
+        if (!saved) throw RuntimeException("Failed to save member with email: ${request.email} to the database")
+
+        val token = tokenService.createToken(request.email)
+        return TokenResponse(token)
     }
 }
