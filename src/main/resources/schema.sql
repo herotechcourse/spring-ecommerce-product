364b5e78bc469aba6e27443ec7681c190c6fabfd
Bojana Petroska <bojana.petroska@yahoo.com>
2025-07-23 15:39:40 +0200

    refactor(database): update schema for uniqueness

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 -- schema.sql
 CREATE TABLE IF NOT EXISTS products (
     id       BIGINT       PRIMARY KEY    AUTO_INCREMENT,
-    name     VARCHAR      NOT NULL,
+    name     VARCHAR      NOT NULL UNIQUE,
     price    DOUBLE       NOT NULL,
     img      VARCHAR(120) NOT NULL,
     quantity INTEGER      NOT NULL
