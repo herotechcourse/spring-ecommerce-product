2cf691f1d202d23a697bca06a5fba65930687a4a
Bojana Petroska <bojana.petroska@yahoo.com>
2025-07-23 15:42:26 +0200

    refactor(service): implement product business logic and exception throwing

--- src/main/kotlin/ecommerce/service/ProductService.kt
+++ src/main/kotlin/ecommerce/service/ProductService.kt
@@ -1,6 +1,8 @@
 package ecommerce.service
 
-import ecommerce.model.Product
+import ecommerce.domain.Product
+import ecommerce.exception.InvalidInputException
+import ecommerce.exception.ResourceNotFoundException
 import ecommerce.repository.ProductRepository
 import org.springframework.stereotype.Service
 
@@ -8,9 +10,15 @@ import org.springframework.stereotype.Service
 class ProductService(private val productRepository: ProductRepository) {
     fun getAllProducts(): List<Product> = productRepository.findAllProducts()
 
-    fun getProductById(id: Long): Product? = productRepository.findById(id)
+    fun getProductById(id: Long): Product {
+        if (id <= 0) throw IllegalArgumentException("Product ID must be greater than or equal to 1")
+        return productRepository.findById(id) ?: throw ResourceNotFoundException("Product", "id", id)
+    }
 
     fun createProduct(newProduct: Product) {
+        if (productRepository.findByName(newProduct.name) != null) {
+            throw InvalidInputException("Product name ${newProduct.name} already exists")
+        }
         productRepository.create(newProduct)
     }
 
@@ -18,7 +26,39 @@ class ProductService(private val productRepository: ProductRepository) {
         id: Long,
         updatedProduct: Product,
     ) {
+        if (id <= 0) throw IllegalArgumentException("Product ID must be greater than or equal to 1")
+
+        val existingProduct =
+            productRepository.findById(id)
+                ?: throw ResourceNotFoundException("Product", "id", id)
+
+        if (updatedProduct.name != existingProduct.name) {
+            if (productRepository.findByName(updatedProduct.name) != null) {
+                throw InvalidInputException("Product with name '${updatedProduct.name}' already exists. Name must be unique.")
+            }
+        }
+
         productRepository.update(id, updatedProduct)
+    }
+
+    fun decreaseProductStock(
+        id: Long,
+        amount: Int,
+    ) {
+        val existingProduct =
+            productRepository.findById(id)
+                ?: throw ResourceNotFoundException("Product", "id", id)
+
+        if (amount <= 0) {
+            throw IllegalArgumentException("Decrease amount must be positive.")
+        }
+
+        if (existingProduct.quantity < amount) {
+            throw IllegalStateException("Decrease amount must be positive.")
+        }
+
+        existingProduct.quantity -= amount
+        productRepository.update(id, existingProduct)
     }
 
     fun deleteProduct(id: Long) = productRepository.delete(id)
