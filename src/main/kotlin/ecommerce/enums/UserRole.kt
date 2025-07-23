657c69a7673fd6274920c0f77ab2397391d9c299
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-07-23 11:06:49 +0200

    feat: Add user roles

--- src/main/kotlin/ecommerce/enums/UserRole.kt
+++ src/main/kotlin/ecommerce/enums/UserRole.kt
@@ -1,6 +1,13 @@
 package ecommerce.enums
 
-enum class UserRole(val role: String) {
+enum class UserRole(val value: String) {
     USER("user"),
     ADMIN("admin"),
+    ;
+
+    companion object {
+        fun fromValue(value: String): UserRole =
+            entries.find { it.value == value }
+                ?: throw IllegalArgumentException("Unknown role: $value")
+    }
 }
