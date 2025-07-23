d65fe2d3086bd6626018601f866e0efa77c6fac8
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-23 20:01:31 +0200

    refactor: adapt registerMember to inject the hashed password

--- src/main/kotlin/ecommerce/service/AuthenticationService.kt
+++ src/main/kotlin/ecommerce/service/AuthenticationService.kt
@@ -1,10 +1,12 @@
 package ecommerce.service
 
 import ecommerce.configuration.JwtTokenProvider
+import ecommerce.configuration.PasswordEncoder
 import ecommerce.dto.RegistrationRequest
 import ecommerce.dto.TokenResponse
 import ecommerce.exception.EmailOrPasswordIncorrectException
 import ecommerce.exception.MemberEmailAlreadyExistsException
+import ecommerce.model.Member
 import ecommerce.repository.MemberRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -13,13 +15,18 @@ import org.springframework.transaction.annotation.Transactional
 class AuthenticationService(
     private val memberRepository: MemberRepository,
     private val tokenService: JwtTokenProvider,
+    private val passwordEncoder: PasswordEncoder,
 ) {
     @Transactional
     fun registerMember(request: RegistrationRequest): TokenResponse {
         if (memberRepository.existsByEmail(request.email)) {
             throw MemberEmailAlreadyExistsException("Email already exists: ${request.email}")
         }
-        val saved = memberRepository.save(request)
+
+        val hashedPassword = passwordEncoder.encode(request.password)
+        val member = Member(null, request.email, hashedPassword)
+
+        val saved = memberRepository.save(member)
 
         if (!saved) throw RuntimeException("Failed to save member with email: ${request.email} to the database")
 
