f1fbb78206b4ffee23ecd315a0ce5b5a34faa188
Mina Kim <mina.kim@deliveryhero.com>
2025-07-23 15:48:38 +0200

    refactor(resources, product-request): change image url limitation to 2000

--- src/main/kotlin/ecommerce/product/data/ConstantsProduct.kt
+++ src/main/kotlin/ecommerce/product/data/ConstantsProduct.kt
@@ -5,7 +5,7 @@ object ConstantsProduct {
         // letters, digits, space, (), [], +, -, &, /, _
         const val NAME_ALLOWED_PATTERN = "^[\\w\\s\\[\\]\\(\\)\\+\\-&/_]*$"
         const val NAME_MAX_LENGTH = 15
-        const val IMAGE_URL_MAX_LENGTH = 255
+        const val IMAGE_URL_MAX_LENGTH = 2000
         const val PRICE_MIN = "0.10"
         const val PRICE_DECIMAL_SCALE = 2
         const val IMAGE_URL_PATTERN = "^(http|https)://.*$"
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,5 +3,5 @@ CREATE TABLE products
     id        LONG PRIMARY KEY AUTO_INCREMENT UNIQUE,
     name      VARCHAR(255) UNIQUE,
     price     DECIMAL(10, 2),
-    image_url VARCHAR(255)
+    image_url VARCHAR(2000)
 );
