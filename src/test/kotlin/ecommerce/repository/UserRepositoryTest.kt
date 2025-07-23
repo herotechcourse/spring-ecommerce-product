1bf017e40196b84640f2e15eea21e742c4d600aa
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-07-23 13:32:47 +0200

    test: User repo test

---
+++ src/test/kotlin/ecommerce/repository/UserRepositoryTest.kt
@@ -0,0 +1,67 @@
+package ecommerce.repository
+
+import ecommerce.dto.UserDTO
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.assertj.core.api.Assertions.assertThat
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class UserRepositoryTest {
+    @Autowired
+    lateinit var userRepository: UserRepository
+
+    @Test fun existsByEmail() {
+        val user = UserDTO(
+            name = "test",
+            password = "test123",
+            email = "existsByEmail@test.com",
+        )
+        userRepository.create(user)
+        val existsByEmail = userRepository.existsByEmail("existsByEmail@test.com")
+        assertThat(existsByEmail).isTrue
+    }
+
+    @Test fun `returns false if not existsByEmail`() {
+        val existsByEmail = userRepository.existsByEmail("none@test.com")
+        assertThat(existsByEmail).isFalse
+    }
+
+    @Test
+    fun create() {
+        val user = UserDTO(
+            name = "test",
+            password = "test123",
+            email = "create@test.com",
+        )
+        userRepository.create(user)
+        val existsByEmail = userRepository.existsByEmail("create@test.com")
+        assertThat(existsByEmail).isTrue
+    }
+
+    @Test
+    fun findByEmailAndPassword() {
+        val userDTO = UserDTO(
+            name = "test",
+            password = "test123",
+            email = "findByEmailAndPassword@test.com",
+        )
+        userRepository.create(userDTO)
+
+        val user = userRepository.findByEmailAndPassword(userDTO.email, userDTO.password)
+        assertThat(user).isNotNull
+    }
+
+    @Test
+    fun `returns null for findByEmailAndPassword`() {
+        val userDTO = UserDTO(
+            name = "test",
+            password = "test123",
+            email = "findByEmailAndPasswordFalse@test.com",
+        )
+        userRepository.create(userDTO)
+
+        val user = userRepository.findByEmailAndPassword(userDTO.email, "")
+        assertThat(user).isNull()
+    }
+}
