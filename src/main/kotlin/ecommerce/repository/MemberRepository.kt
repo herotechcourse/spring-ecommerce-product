db36a3d5344806d9956f4755fdbded2d6c2bb482
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-23 16:11:08 +0200

    feat: implement RowMapper in db and findByEmail

--- src/main/kotlin/ecommerce/repository/MemberRepository.kt
+++ src/main/kotlin/ecommerce/repository/MemberRepository.kt
@@ -1,13 +1,25 @@
 package ecommerce.repository
 
 import ecommerce.dto.RegistrationRequest
+import ecommerce.model.Member
+import org.springframework.jdbc.core.RowMapper
 import org.springframework.jdbc.core.simple.JdbcClient
 import org.springframework.jdbc.support.GeneratedKeyHolder
 import org.springframework.jdbc.support.KeyHolder
 import org.springframework.stereotype.Repository
+import java.sql.ResultSet
 
 @Repository
 class MemberRepository(private val db: JdbcClient) {
+    private val memberRowMapper =
+        RowMapper<Member> { rs: ResultSet, _ ->
+            Member(
+                rs.getLong("id"),
+                rs.getString("email"),
+                rs.getString("password"),
+            )
+        }
+
     fun save(member: RegistrationRequest): Boolean {
         val keyHolder: KeyHolder = GeneratedKeyHolder()
 
@@ -24,5 +36,20 @@ class MemberRepository(private val db: JdbcClient) {
             .param(email)
             .query(Int::class.java)
             .single() > 0
+    }
+
+    fun findByEmail(email: String): Member? {
+        return db
+            .sql("SELECT * FROM members WHERE email = ?")
+            .param(email)
+            .query { rs, _ ->
+                Member(
+                    id = rs.getLong("id"),
+                    email = rs.getString("email"),
+                    password = rs.getString("password"),
+                )
+            }
+            .optional()
+            .orElse(null)
     }
 }
