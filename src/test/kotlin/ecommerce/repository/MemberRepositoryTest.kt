1cc8be9345620a35d99577683e661b70b9928921
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-23 12:04:56 +0200

    test(MemberRepository): add test to save new Member function

---
+++ src/test/kotlin/ecommerce/repository/MemberRepositoryTest.kt
@@ -0,0 +1,38 @@
+package ecommerce.repository
+
+import ecommerce.dto.RegistrationRequest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest
+import org.springframework.jdbc.core.simple.JdbcClient
+
+@JdbcTest
+class MemberRepositoryTest {
+    private lateinit var memberRepository: MemberRepository
+
+    @Autowired
+    private lateinit var db: JdbcClient
+
+    @BeforeEach
+    fun setUp() {
+        memberRepository = MemberRepository(db)
+        val sql = "CREATE TABLE members (id INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255), password VARCHAR(255))"
+        db.sql(sql).update()
+    }
+
+    @Test
+    fun save() {
+        val user = RegistrationRequest("test@example.com", "test123")
+        val saved = memberRepository.save(user)
+        assertTrue(saved)
+        val count =
+            db.sql("SELECT COUNT(*) FROM members")
+                .query(Int::class.java)
+                .single()
+
+        assertThat(count).isEqualTo(1)
+    }
+}
