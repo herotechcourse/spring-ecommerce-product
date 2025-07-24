a4beb8e7849dc0e5c545229463c5ce7284ba96be
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-24 20:39:48 +0200

    feat: implement LoginMemberArgumentResolver

---
+++ src/main/kotlin/ecommerce/configuration/LoginMemberArgumentResolver.kt
@@ -0,0 +1,34 @@
+package ecommerce.configuration
+
+import ecommerce.annotation.LoginMember
+import ecommerce.model.Member
+import ecommerce.repository.MemberRepository
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.core.MethodParameter
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.support.WebDataBinderFactory
+import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.method.support.ModelAndViewContainer
+import javax.naming.AuthenticationException
+
+@Component
+class LoginMemberArgumentResolver(
+    private val memberRepository: MemberRepository,
+) : HandlerMethodArgumentResolver {
+    override fun supportsParameter(parameter: MethodParameter): Boolean {
+        return parameter.hasParameterAnnotation(LoginMember::class.java)
+    }
+
+    override fun resolveArgument(
+        parameter: MethodParameter,
+        mavContainer: ModelAndViewContainer?,
+        webRequest: NativeWebRequest,
+        binderFactory: WebDataBinderFactory?,
+    ): Member {
+        val request = webRequest.getNativeRequest(HttpServletRequest::class.java) ?: throw AuthenticationException("No HttpServletRequest")
+        val email = request.getAttribute("email") as? String ?: throw AuthenticationException("No authenticated member found 1")
+        val member = memberRepository.findByEmail(email) ?: throw AuthenticationException("No authenticated member found 2")
+        return member
+    }
+}
