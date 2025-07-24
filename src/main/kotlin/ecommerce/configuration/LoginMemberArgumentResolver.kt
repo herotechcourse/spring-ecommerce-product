6b8154ed85f8bced5a896a009572a9c3b587a71e
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-24 23:36:14 +0200

    refactor: throw Unauthorized Exception instead od Authorization in the resolver if request, email or member not found

--- src/main/kotlin/ecommerce/configuration/LoginMemberArgumentResolver.kt
+++ src/main/kotlin/ecommerce/configuration/LoginMemberArgumentResolver.kt
@@ -2,6 +2,7 @@ package ecommerce.configuration
 
 import ecommerce.annotation.LoginMember
 import ecommerce.dto.MemberDto
+import ecommerce.exception.UnauthorizedException
 import ecommerce.repository.MemberRepository
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.core.MethodParameter
@@ -10,7 +11,6 @@ import org.springframework.web.bind.support.WebDataBinderFactory
 import org.springframework.web.context.request.NativeWebRequest
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.method.support.ModelAndViewContainer
-import javax.naming.AuthenticationException
 
 @Component
 class LoginMemberArgumentResolver(
@@ -26,9 +26,9 @@ class LoginMemberArgumentResolver(
         webRequest: NativeWebRequest,
         binderFactory: WebDataBinderFactory?,
     ): MemberDto {
-        val request = webRequest.getNativeRequest(HttpServletRequest::class.java) ?: throw AuthenticationException("No HttpServletRequest")
-        val email = request.getAttribute("email") as? String ?: throw AuthenticationException("No authenticated member found 1")
-        val member = memberRepository.findByEmail(email) ?: throw AuthenticationException("No authenticated member found 2")
+        val request = webRequest.getNativeRequest(HttpServletRequest::class.java) ?: throw UnauthorizedException("No HttpServletRequest")
+        val email = request.getAttribute("email") as? String ?: throw UnauthorizedException("No authenticated member found")
+        val member = memberRepository.findByEmail(email) ?: throw UnauthorizedException("No authenticated member found")
         return member.toDto()
     }
 }
