61bc9392a65f374792aac76b35cf76facfa71962
pebencze <petra.bencze@deliveryhero.com>
2025-07-24 09:59:36 +0200

    refactor(NotfoundException, UnauthorizedException): allow message to be null

--- src/main/kotlin/ecommerce/exception/UnauthorizedException.kt
+++ src/main/kotlin/ecommerce/exception/UnauthorizedException.kt
@@ -4,4 +4,4 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
-class UnauthorizedException(override val message: String?) : RuntimeException()
+class UnauthorizedException(override val message: String? = null) : RuntimeException()
