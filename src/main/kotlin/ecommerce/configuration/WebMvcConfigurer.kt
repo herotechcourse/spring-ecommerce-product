ed571ac1304d7846b7dda2763d1b09e6dbe4759d
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-24 20:39:16 +0200

    feat: add addArgumentResolvers in WebMvcConfiguration

--- src/main/kotlin/ecommerce/configuration/WebMvcConfigurer.kt
+++ src/main/kotlin/ecommerce/configuration/WebMvcConfigurer.kt
@@ -1,15 +1,21 @@
 package ecommerce.configuration
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebMvcConfigurer(
     private val jwtAuthInterceptor: JwtAuthInterceptor,
+    private val loginMemberArgumentResolver: LoginMemberArgumentResolver,
 ) : WebMvcConfigurer {
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(jwtAuthInterceptor)
-            .addPathPatterns("")
+            .addPathPatterns("/api/user/wishes")
+    }
+
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver?>) {
+        resolvers.add(loginMemberArgumentResolver)
     }
 }
