357695e41efc469d798865338c6316cf246ca90c
arekiu <alexis.aguilera@deliveryhero.com>
2025-07-24 15:34:54 +0200

    feat(MemberService): add validation to check that email is unique

--- src/main/kotlin/ecommerce/auth/service/MemberService.kt
+++ src/main/kotlin/ecommerce/auth/service/MemberService.kt
@@ -1,12 +1,21 @@
 package ecommerce.auth.service
 
+import ecommerce.auth.application.AuthException
 import ecommerce.auth.model.Member
 import ecommerce.auth.store.MemberStore
 import org.springframework.stereotype.Service
 
 @Service
 class MemberService(private val memberStore: MemberStore) {
-    fun create(member: Member): Member = memberStore.createMember(member)
+
+    fun create(member: Member): Member {
+        val allMembers = findAll()
+        val emailAlreadyExists = allMembers.any { it.email == member.email }
+        if (emailAlreadyExists) {
+            throw AuthException()
+        }
+        return memberStore.createMember(member)
+    }
 
     fun findAll(): List<Member> = memberStore.findAllMembers()
 
