66faa54d3111d519013abbe4e535bc2e775ce45b
Sanghun Park <po4865@gmail.com>
2025-07-24 20:59:45 +0200

    feat(ProductController): add form validation using 'ProductForm' with '@Valid' and 'BindingResult', and apply 'ProductService'

--- src/main/kotlin/ecommerce/api/ProductController.kt
+++ src/main/kotlin/ecommerce/api/ProductController.kt
@@ -1,8 +1,11 @@
 package ecommerce.api
 
-import ecommerce.dao.JdbcProductDAO
+import ecommerce.ProductForm
 import ecommerce.model.Product
+import ecommerce.service.ProductService
+import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -11,30 +14,28 @@ import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.net.URI
 
 @RestController
 @RequestMapping("/api")
-class ProductController(private val jdbcProductDao: JdbcProductDAO) {
+class ProductController(private val productService: ProductService) {
     @PostMapping("/products")
     fun createProduct(
-        @RequestBody product: Product,
-    ): ResponseEntity<Product> {
-        var id: Long
-        try {
-            id = jdbcProductDao.insert(product)
-        } catch (exception: Exception) {
-            println("createProduct(): " + exception.message)
-            return ResponseEntity.internalServerError().build()
+        @RequestBody @Valid productForm: ProductForm,
+        bindingResult: BindingResult,
+    ): ResponseEntity<Any> {
+        if (bindingResult.hasErrors()) {
+            val errors =
+                bindingResult.fieldErrors.associate { error ->
+                    error.field to (error.defaultMessage ?: "Invalid value")
+                }
+            return ResponseEntity.badRequest().body(errors)
         }
-        val insertedProduct = jdbcProductDao.findById(id)
-        val uri = URI.create("/api/products/$id")
-        return ResponseEntity.created(uri).body(insertedProduct)
+        return productService.insert(productForm)
     }
 
     @GetMapping("/products")
     fun getProducts(): ResponseEntity<List<Product>> {
-        val products = jdbcProductDao.findAll()
+        val products = productService.findAll()
         return ResponseEntity.ok(products)
     }
 
@@ -42,7 +43,7 @@ class ProductController(private val jdbcProductDao: JdbcProductDAO) {
     fun getProduct(
         @PathVariable id: Long,
     ): ResponseEntity<Product> {
-        val product = jdbcProductDao.findById(id) ?: return ResponseEntity.notFound().build()
+        val product = productService.findById(id) ?: return ResponseEntity.notFound().build()
         return ResponseEntity.ok(product)
     }
 
@@ -52,10 +53,10 @@ class ProductController(private val jdbcProductDao: JdbcProductDAO) {
         @RequestBody newProduct: Product,
     ): ResponseEntity<Product> {
         val product = Product.toEntity(newProduct, id)
-        val result = jdbcProductDao.update(product)
+        val result = productService.update(product)
         if (result == 1) {
-            val product = jdbcProductDao.findById(id) ?: return ResponseEntity.notFound().build()
-            return ResponseEntity.ok(product)
+            val target = productService.findById(id) ?: return ResponseEntity.notFound().build()
+            return ResponseEntity.ok(target)
         }
         return ResponseEntity.notFound().build()
     }
@@ -64,8 +65,8 @@ class ProductController(private val jdbcProductDao: JdbcProductDAO) {
     fun deleteProduct(
         @PathVariable id: Long,
     ): ResponseEntity<Void> {
-        jdbcProductDao.findById(id) ?: return ResponseEntity.notFound().build()
-        jdbcProductDao.delete(id)
+        productService.findById(id) ?: return ResponseEntity.notFound().build()
+        productService.delete(id)
         return ResponseEntity.noContent().build()
     }
 }
--- src/test/kotlin/ecommerce/api/ProductControllerTest.kt
+++ src/test/kotlin/ecommerce/api/ProductControllerTest.kt
@@ -2,25 +2,34 @@ package ecommerce.api
 
 import ecommerce.dao.JdbcProductDAO
 import ecommerce.model.Product
+import ecommerce.service.ProductService
+import io.restassured.RestAssured
+import io.restassured.http.ContentType
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpStatus
 import org.springframework.jdbc.core.JdbcTemplate
 
-@JdbcTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class ProductControllerTest {
-    private lateinit var jdbcProductDao: JdbcProductDAO
+    @Autowired private lateinit var jdbcTemplate: JdbcTemplate
 
-    @Autowired
-    private lateinit var jdbcTemplate: JdbcTemplate
+    @Autowired private lateinit var jdbcProductDao: JdbcProductDAO
+    private lateinit var productService: ProductService
     private lateinit var controller: ProductController
+
+    fun create() {
+        val product = Product(name = "product1", price = 1.5, imageUrl = "https://www.product.com/image/1")
+        jdbcProductDao.insert(product)
+    }
 
     @BeforeEach
     fun setUp() {
         jdbcProductDao = JdbcProductDAO(jdbcTemplate)
+        productService = ProductService(jdbcProductDao)
 
         jdbcTemplate.execute("DROP TABLE product CASCADE")
         jdbcTemplate.execute(
@@ -40,15 +49,142 @@ class ProductControllerTest {
                     INSERT INTO product (name, price, imageUrl) VALUES ('Full Metal Alchemist', 1000, 'https://alexnsan.comics/imageurl/5');"""
         jdbcTemplate.batchUpdate(query)
 
-        controller = ProductController(jdbcProductDao)
+        controller = ProductController(productService)
     }
 
     @Test
-    fun create() {
-        val product = Product(name = "product1", price = 1.5, imageUrl = "https://www.product.com/image/1")
-        val response = controller.createProduct(product)
+    fun `create() - should insert product and return 201 when form is valid`() {
+        val response =
+            RestAssured
+                .given().log().all()
+                .body(Product(name = "product1 [new]", price = 1.5, imageUrl = "https://www.product.com/image/1"))
+                .contentType(ContentType.JSON)
+                .`when`().post("/api/products")
+                .then().log().all().extract()
 
-        assertThat(response.statusCode.value()).isEqualTo(HttpStatus.CREATED.value())
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value())
+    }
+
+    @Test
+    fun `create() - should return 400 when name is blank`() {
+        val response =
+            RestAssured
+                .given().log().all()
+                .body(Product(name = "", price = 1.5, imageUrl = "https://www.product.com/image/1"))
+                .contentType(ContentType.JSON)
+                .`when`().post("/api/products")
+                .then().log().all().extract()
+
+        val targets =
+            listOf(
+                "Contains unallowed character",
+                "Product name is required",
+                "Must be no more than 15 characters, including spaces",
+            )
+        val actual = response.jsonPath().getString("name")
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value())
+        assertThat(actual).isIn(targets)
+    }
+
+    @Test
+    fun `create() - should return 400 when name is more than 15 characters`() {
+        val response =
+            RestAssured
+                .given().log().all()
+                .body(Product(name = "this is very long name", price = 1.5, imageUrl = "https://www.product.com/image/1"))
+                .contentType(ContentType.JSON)
+                .`when`().post("/api/products")
+                .then().log().all().extract()
+
+        val expected = "Must be no more than 15 characters, including spaces"
+        val actual = response.jsonPath().getString("name")
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value())
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `create() - should return 400 when name has unallowed character`() {
+        val response =
+            RestAssured
+                .given().log().all()
+                .body(Product(name = "I am product!", price = 1.5, imageUrl = "https://www.product.com/image/1"))
+                .contentType(ContentType.JSON)
+                .`when`().post("/api/products")
+                .then().log().all().extract()
+
+        val expected = "Contains unallowed character"
+        val actual = response.jsonPath().getString("name")
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value())
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `create() - should return 400 when price is 0`() {
+        val response =
+            RestAssured
+                .given().log().all()
+                .body(Product(name = "base", price = 0.0, imageUrl = "https://www.product.com/image/1"))
+                .contentType(ContentType.JSON)
+                .`when`().post("/api/products")
+                .then().log().all().extract()
+
+        val expected = "Product price must be greater than zero"
+        val actual = response.jsonPath().getString("price")
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value())
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `create() - should return 400 when image URL is blank`() {
+        val response =
+            RestAssured
+                .given().log().all()
+                .body(Product(name = "base", price = 2.0, imageUrl = ""))
+                .contentType(ContentType.JSON)
+                .`when`().post("/api/products")
+                .then().log().all().extract()
+
+        val expected =
+            listOf(
+                "Product image URL is required",
+                "Must start with 'https://'.",
+            )
+        val actual = response.jsonPath().getString("imageUrl")
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value())
+        assertThat(actual).isIn(expected)
+    }
+
+    @Test
+    fun `create() - should return 400 when email is not valid`() {
+        val response =
+            RestAssured
+                .given().log().all()
+                .body(Product(name = "base", price = 2.0, imageUrl = "ssh://www.product.com/image/1"))
+                .contentType(ContentType.JSON)
+                .`when`().post("/api/products")
+                .then().log().all().extract()
+
+        val expected = "Must start with 'https://'."
+        val actual = response.jsonPath().getString("imageUrl")
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value())
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `create() - should return 400 when name of product already exists`() {
+        val name = "Iron Man"
+        val response =
+            RestAssured
+                .given().log().all()
+                .body(Product(name = name, price = 2.0, imageUrl = "https://www.product.com/image/1"))
+                .contentType(ContentType.JSON)
+                .`when`().post("/api/products")
+                .then().log().all().extract()
+
+        val expected = "Product with name '$name' already exists."
+        val actual = response.jsonPath().getString("name")
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value())
+        assertThat(actual).isEqualTo(expected)
     }
 
     @Test
