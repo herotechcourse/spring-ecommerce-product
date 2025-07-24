3cda90535b6de7c9238bf6f15e826e9ae280cb8f
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-24 19:49:57 +0200

    refactor: call addProductToCart from the controller. Return a Result, PRODUCT_QUANTITY_UPDATED or PRODUCT_ADDED

--- src/main/kotlin/ecommerce/controller/CartController.kt
+++ src/main/kotlin/ecommerce/controller/CartController.kt
@@ -1,20 +1,24 @@
 package ecommerce.controller
 
 import ecommerce.dto.CartItemRequest
-import ecommerce.model.Member
+import ecommerce.dto.CartUpdateResult
+import ecommerce.service.CartService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/wishes")
-class CartController {
+@RequestMapping("/api/user/wishes")
+class CartController(
+    private val cartService: CartService,
+) {
     @PostMapping
     fun addToCart(
         @RequestBody request: CartItemRequest,
-        member: Member,
-    ) {
-        // Add product to cart for this member
+    ): ResponseEntity<CartUpdateResult> {
+        val addToCartResult = cartService.addProductToCart(1L, request.productId, request.quantity)
+        return ResponseEntity.ok(addToCartResult)
     }
 }
