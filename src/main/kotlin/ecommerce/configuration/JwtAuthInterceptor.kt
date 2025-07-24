42f4b0c6b3cdef28f662cc1981d4706dbdd378a3
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-24 15:18:39 +0200

    feat: add implementation JwtAuthInterceptor

---
+++ src/main/kotlin/ecommerce/configuration/JwtAuthInterceptor.kt
@@ -0,0 +1,24 @@
+package ecommerce.configuration
+
+import ecommerce.exception.UnauthorizedException
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.web.servlet.HandlerInterceptor
+
+class JwtAuthInterceptor(
+    val jwtTokenProvider: JwtTokenProvider,
+) : HandlerInterceptor {
+    override fun preHandle(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+    ): Boolean {
+        val accessToken = request.getHeader("Authorization")?.substringAfter("Bearer ")
+        if (accessToken == null) throw UnauthorizedException("Invalid Token")
+        if (!jwtTokenProvider.validateToken(accessToken)) throw UnauthorizedException("Invalid Token")
+
+        val email = jwtTokenProvider.getEmailFromToken(accessToken)
+        request.setAttribute("email", email)
+        return true
+    }
+}
