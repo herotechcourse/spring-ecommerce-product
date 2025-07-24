8a8a0af7c9c741677f69ec2146402feba3c80082
arekiu <alexis.aguilera@deliveryhero.com>
2025-07-24 15:23:05 +0200

    feat(AuthException): create custom exception

---
+++ src/main/kotlin/ecommerce/auth/application/AuthException.kt
@@ -0,0 +1,7 @@
+package ecommerce.auth.application
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+class AuthException : RuntimeException()
