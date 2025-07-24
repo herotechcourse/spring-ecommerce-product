41e5d9f2d097bc6fcbbd9edb3fd58c97ace4f7d6
arekiu <alexis.aguilera@deliveryhero.com>
2025-07-24 15:24:21 +0200

    feat(MemberController): create login and register endpoints

--- src/main/kotlin/ecommerce/auth/controller/MemberController.kt
+++ src/main/kotlin/ecommerce/auth/controller/MemberController.kt
@@ -1,25 +1,50 @@
 package ecommerce.auth.controller
 
+import ecommerce.auth.application.AuthService
+import ecommerce.auth.infrastructure.AuthorizationExtractor
+import ecommerce.auth.infrastructure.BearerAuthorizationExtractor
 import ecommerce.auth.model.Member
 import ecommerce.auth.model.MemberDTO
+import ecommerce.auth.model.TokenRequest
+import ecommerce.auth.model.TokenResponse
 import ecommerce.auth.service.MemberService
+import jakarta.servlet.http.HttpServletRequest
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.net.URI
 
 @RestController
-@RequestMapping("/api")
-class MemberController(private val userService: MemberService) {
-    @PostMapping("/members/register")
+@RequestMapping("/api/members")
+class MemberController(
+    private val userService: MemberService,
+    private val authService: AuthService,
+) {
+    private val authorizationExtractor: AuthorizationExtractor<String> = BearerAuthorizationExtractor()
+
+    @PostMapping("/register")
     fun registerUser(
         @Valid @RequestBody member: Member,
-    ): ResponseEntity<MemberDTO> {
+    ): ResponseEntity<TokenResponse> {
         val created = userService.create(member)
         val dto = MemberDTO.from(created)
-        return ResponseEntity.created(URI("/users/${dto.id}")).body(dto)
+        val tokenRequest = TokenRequest(member.email, member.password)
+        val tokenResponse = authService.createToken(tokenRequest)
+        return ResponseEntity.created(URI("/users/${dto.id}")).body(tokenResponse)
+    }
+
+    @PostMapping("/login")
+    fun loginUser(
+        @RequestBody tokenRequest: TokenRequest,
+    ): ResponseEntity<TokenResponse> {
+        val tokenResponse = authService.issueTokenToLoggedUSer(tokenRequest)
+        return ResponseEntity.ok().body(tokenResponse)
+    }
+
+    @GetMapping("/me/token")
+    fun findMyInfo(request: HttpServletRequest): ResponseEntity<MemberDTO> {
+        val token = authorizationExtractor.extract(request)
+        val member = authService.findMemberByToken(token)
+        return ResponseEntity.ok().body(member)
     }
 }
