08a55ad394110431fd5a93ae20036514d60425e5
Bojana Petroska <bojana.petroska@yahoo.com>
2025-07-24 15:22:16 +0200

    feat(cart): implement cart and cart item domain, schema, and repositories
    Key changes include:
    - Domain:
        - Added `Cart` entity
        - Added `CartItem` entity
    - Schema:
        - Created `carts` table
        - Created `cart_items` table
    - Repositories:
        - Implemented `CartRepository`
        - Implemented `CartItemRepository`

---
+++ src/main/kotlin/ecommerce/domain/CartItem.kt
@@ -0,0 +1,3 @@
+package ecommerce.domain
+
+data class CartItem(var id: Long = 0, val cartId: Long, val productId: Long, val quantity: Int)
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,12 +5,28 @@ CREATE TABLE IF NOT EXISTS products (
     price    DOUBLE       NOT NULL,
     img      VARCHAR(120) NOT NULL,
     quantity INTEGER      NOT NULL
-    );
+);
 
 CREATE TABLE IF NOT EXISTS members (
-    userId   BIGINT       PRIMARY KEY    AUTO_INCREMENT,
-    userName VARCHAR      NOT NULL,
+    user_id   BIGINT       PRIMARY KEY    AUTO_INCREMENT,
+    user_name VARCHAR      NOT NULL,
     email    VARCHAR      NOT NULL UNIQUE,
-    passwordHash VARCHAR(120) NOT NULL,
+    password_hash VARCHAR(120) NOT NULL,
     role VARCHAR      NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS carts (
+    id           BIGINT     PRIMARY KEY    AUTO_INCREMENT,
+    member_id     BIGINT     NOT NULL UNIQUE,
+    created_at    TIMESTAMP  NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES members(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS cart_items (
+    id      BIGINT AUTO_INCREMENT PRIMARY KEY,
+    cart_id BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    quantity INT NOT NULL,
+    FOREIGN KEY (cart_id) REFERENCES carts(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
 );
