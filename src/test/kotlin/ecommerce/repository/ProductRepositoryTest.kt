6989e9aa7c6ebb407a4a9ccbd668037b89711e23
Bojana Petroska <bojana.petroska@yahoo.com>
2025-07-25 18:27:14 +0200

    refactor(test): adjust the tests to work with authentication

--- src/test/kotlin/ecommerce/repository/ProductRepositoryTest.kt
+++ src/test/kotlin/ecommerce/repository/ProductRepositoryTest.kt
@@ -2,64 +2,71 @@ package ecommerce.repository
 
 import ecommerce.domain.Product
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest
 import org.springframework.context.annotation.Import
-import org.springframework.test.annotation.DirtiesContext
+import org.springframework.jdbc.core.JdbcTemplate
 
 @JdbcTest
 @Import(ProductRepository::class)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+// @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ProductRepositoryTest
     @Autowired
     constructor(
         private val productRepository: ProductRepository,
+        private val jdbcTemplate: JdbcTemplate,
     ) {
+        @BeforeEach
+        fun setUp() {
+            jdbcTemplate.execute("delete from products")
+        }
+
         @Test
         fun `it should count the products`() {
-            productRepository.create(Product(id = 1, "test", 20.0, "http://astr", 20))
-            productRepository.create(Product(id = 2, "test2", 20.0, "http://astr", 20))
-            productRepository.create(Product(id = 3, "test3", 20.0, "http://astr", 20))
+            val p1 = productRepository.create(Product(name = "test", price = 20.0, img = "http://astr", quantity = 20))
+            val p2 = productRepository.create(Product(name = "test2", price = 20.0, img = "http://astr", quantity = 20))
+            val p3 = productRepository.create(Product(name = "test3", price = 20.0, img = "http://astr", quantity = 20))
 
             assertThat(productRepository.count()).isEqualTo(3)
+            assertThat(p1.id).isGreaterThan(0)
+            assertThat(p2.id).isGreaterThan(p1.id)
+            assertThat(p3.id).isGreaterThan(p2.id)
         }
 
         @Test
         fun `it should find a product by id`() {
-            productRepository.create(Product(id = 1, "test", 20.0, "http://astr", 20))
-            assertThat(productRepository.findById(1)).isNotNull()
-            assertThat(productRepository.findById(1)).isEqualTo(Product(id = 1, "test", 20.0, "http://astr", 20))
+            val createdProduct =
+                productRepository.create(Product(name = "test", price = 20.0, img = "http://astr", quantity = 20))
+            assertThat(productRepository.findById(createdProduct.id)).isNotNull()
+            assertThat(productRepository.findById(createdProduct.id)).isEqualTo(createdProduct)
         }
 
         @Test
         fun `it should return all products`() {
-            productRepository.create(Product(id = 1, "test", 20.0, "http://astr", 20))
-            productRepository.create(Product(id = 2, "test2", 20.0, "http://astr", 20))
-            productRepository.create(Product(id = 3, "test3", 20.0, "http://astr", 20))
+            val p1 = productRepository.create(Product(name = "test", price = 20.0, img = "http://astr", quantity = 20))
+            val p2 = productRepository.create(Product(name = "test2", price = 20.0, img = "http://astr", quantity = 20))
+            val p3 = productRepository.create(Product(name = "test3", price = 20.0, img = "http://astr", quantity = 20))
 
-            assertThat(productRepository.findAllProducts()).isEqualTo(
-                listOf(
-                    Product(id = 1, "test", 20.0, "http://astr", 20),
-                    Product(id = 2, "test2", 20.0, "http://astr", 20),
-                    Product(id = 3, "test3", 20.0, "http://astr", 20),
-                ).toList(),
-            )
+            assertThat(productRepository.findAllProducts()).isEqualTo(listOf(p1, p2, p3).toList())
         }
 
         @Test
         fun `it should update the product properties`() {
-            productRepository.create(Product(id = 1, "test", 20.0, "http://astr", 20))
-            val newProduct = Product(id = 1, "test", 20.0, "http://IMG", 20)
-            productRepository.update(id = 1, product = newProduct)
-            assertThat(productRepository.findById(1)).isEqualTo(newProduct)
-            assertThat(productRepository.findById(1)?.img).isEqualTo(newProduct.img)
+            val createdProduct = productRepository.create(Product(name = "test", price = 20.0, img = "http://astr", quantity = 20))
+            val newUpdatedProduct = createdProduct.copy(name = "test2", price = 30.0, img = "http://astrooooo", quantity = 25)
+            productRepository.update(createdProduct.id, product = newUpdatedProduct)
+            assertThat(productRepository.findById(createdProduct.id)).isEqualTo(newUpdatedProduct)
+            assertThat(productRepository.findById(createdProduct.id)?.img).isEqualTo(newUpdatedProduct.img)
         }
 
         @Test
         fun `it should delete a product by it's id`() {
-            productRepository.create(Product(id = 1, "test", 20.0, "http://astr", 20))
-            productRepository.delete(id = 1)
+            val createdProduct = productRepository.create(Product(name = "test", price = 20.0, img = "http://astr", quantity = 20))
+            productRepository.delete(createdProduct.id)
             assertThat(productRepository.count()).isEqualTo(0)
             assertThat(productRepository.findById(1)).isNull()
         }
