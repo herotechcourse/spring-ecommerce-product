e24fd9f6de47cba9c6d86fd2abe4d238a3534af5
Mina Kim <mina.kim@deliveryhero.com>
2025-07-25 13:56:23 +0200

    feat(token-provider): implement token provider
    - get secret key and validity milliseconds from application.yml

---
+++ src/main/kotlin/ecommerce/auth/service/JwtTokenProvider.kt
@@ -0,0 +1,64 @@
+package ecommerce.auth.service
+
+import ecommerce.auth.data.Member
+import io.jsonwebtoken.JwtException
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.util.Base64
+import java.util.Date
+import javax.crypto.SecretKey
+
+@Component
+class JwtTokenProvider {
+    @Value("\${security.jwt.token.secret-key}")
+    private lateinit var rawSecretKey: String
+
+    @Value("\${security.jwt.token.expire-length}")
+    private var validityInMilliseconds: Long = 0
+
+    private val algorithm = Jwts.SIG.HS256
+
+    private val secretKey: SecretKey by lazy {
+        Keys.hmacShaKeyFor(Base64.getDecoder().decode(rawSecretKey))
+    }
+
+    fun createToken(member: Member): String {
+        val now = Date()
+        val validity = Date(now.time + validityInMilliseconds)
+
+        return Jwts.builder()
+            .subject(member.id.toString())
+            .claim("email", member.email)
+            .issuedAt(now)
+            .expiration(validity)
+            .signWith(secretKey, algorithm)
+            .compact()
+    }
+
+    fun getPayload(token: String): String {
+        val claims =
+            Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .payload
+        return claims.subject
+    }
+
+    fun validateToken(token: String): Boolean {
+        return try {
+            val claims =
+                Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token)
+            !claims.payload.expiration.before(Date())
+        } catch (e: JwtException) {
+            false
+        } catch (e: IllegalArgumentException) {
+            false
+        }
+    }
+}
