88fd1232eefad20916c52665e06bfc4be88f8f67
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-25 21:13:33 +0200

    test: adapt member repository tests to refactoring

--- src/test/kotlin/ecommerce/repository/MemberRepositoryTest.kt
+++ src/test/kotlin/ecommerce/repository/MemberRepositoryTest.kt
@@ -18,18 +18,21 @@ class MemberRepositoryTest {
     @BeforeEach
     fun setUp() {
         memberRepository = MemberRepository(db)
-        db.sql("DROP TABLE CART_ITEMS IF EXISTS").update()
-        db.sql("DROP TABLE CARTS IF EXISTS").update()
-        db.sql("DROP TABLE CART_ITEMS IF EXISTS").update()
-        db.sql("DROP TABLE MEMBERS IF EXISTS").update()
+        db.sql("DROP TABLE IF EXISTS cart_history").update()
+        db.sql("DROP TABLE IF EXISTS cart_items").update()
+        db.sql("DROP TABLE IF EXISTS carts").update()
+        db.sql("DROP TABLE IF EXISTS members").update()
+
         val sql =
             """
-                        CREATE TABLE members (
-                        id INT AUTO_INCREMENT PRIMARY KEY, 
-            email VARCHAR(255) UNIQUE,
-            password VARCHAR(255), 
-            ROLE VARCHAR(100) NOT NULL DEFAULT 'USER')
+            CREATE TABLE members (
+                id INT AUTO_INCREMENT PRIMARY KEY, 
+                email VARCHAR(255) UNIQUE,
+                password VARCHAR(255), 
+                role VARCHAR(100) NOT NULL DEFAULT 'USER'
+            )
             """.trimIndent()
+
         db.sql(sql).update()
     }
 
