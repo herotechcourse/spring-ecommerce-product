e4ee5e120938050b582a29d60bdc7556aabfaacb
Mina Kim <mina.kim@deliveryhero.com>
2025-07-25 15:38:11 +0200

    chore(validation-message): cleanup code

--- src/main/kotlin/ecommerce/view/ValidationMessages.kt
+++ src/main/kotlin/ecommerce/view/ValidationMessages.kt
@@ -1,14 +1,15 @@
 package ecommerce.view
 
-import ecommerce.constants.ConstantsProduct
+import ecommerce.constants.ConstantsProduct.Validation.IMAGE_URL_MAX_LENGTH
+import ecommerce.constants.ConstantsProduct.Validation.PRICE_DECIMAL_SCALE
 
 object ValidationMessages {
     object Invalid {
         const val NAME_MUST_LENGTH = "Name must be between 1 and 15 characters"
         const val NAME_MUST_PATTERN = "Invalid characters in name"
         const val PRICE_MUST_GREATER = "Price must be greater than 0"
-        const val PRICE_MUST_SCALE = "Price must have up to ${ConstantsProduct.Validation.PRICE_DECIMAL_SCALE}} decimal places"
-        const val IMAGE_URL_MUST_LENGTH = "Name must be between 1 and ${ConstantsProduct.Validation.IMAGE_URL_MAX_LENGTH} characters"
+        const val PRICE_MUST_SCALE = "Price must have up to $PRICE_DECIMAL_SCALE decimal places"
+        const val IMAGE_URL_MUST_LENGTH = "Name must be between 1 and $IMAGE_URL_MAX_LENGTH characters"
         const val IMAGE_URL_MUST_PATTERN = "Image URL must start with `http://` or `https://`"
         const val CAN_NOT_BE_EMPTY = "Can not be empty"
     }
