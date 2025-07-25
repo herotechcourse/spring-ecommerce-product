6a80da323f8db8b39d6182ff224257a3944f422d
Jin Park <jieun.park.e@deliveryhero.com>
2025-07-25 21:44:38 +0200

    style: enhance readability by splitting lines

--- src/main/kotlin/ecommerce/exception/ErrorResponse.kt
+++ src/main/kotlin/ecommerce/exception/ErrorResponse.kt
@@ -1,3 +1,7 @@
 package ecommerce.exception
 
-data class ErrorResponse(val error: String, val message: String)
+data class ErrorResponse(
+    val error: String,
+    val message: String,
+    val fieldErrors: Map<String, String>? = null,
+)
