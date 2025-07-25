6eb98beb139c49c7edc39766e5a5f25558597158
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-25 16:21:40 +0200

    feat: implement statistics repository Implement function to get 5 most added products in cart. Implement function to add product in the table when added to a cart

---
+++ src/main/kotlin/ecommerce/repository/AdminStatsRepository.kt
@@ -0,0 +1,61 @@
+package ecommerce.repository
+
+import ecommerce.dto.TopProductStats
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.RowMapper
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert
+import org.springframework.stereotype.Repository
+import java.sql.Timestamp
+
+@Repository
+class AdminStatsRepository(
+    private val jdbcTemplate: JdbcTemplate,
+) {
+    private val cartItemForMostWanted =
+        RowMapper<TopProductStats> { rs, _ ->
+            TopProductStats(
+                rs.getString("product_name"),
+                rs.getInt("added_count"),
+                rs.getTimestamp("last_added_at"),
+            )
+        }
+
+    fun get5MostAddedProducts(): List<TopProductStats> {
+        val sql =
+            """
+            SELECT 
+                p.name AS product_name,
+                COUNT(*) AS added_count,
+                MAX(ci.created_at) AS last_added_at
+            FROM cart_history ci
+            JOIN products p ON ci.product_id = p.id
+            WHERE ci.created_at >= CURRENT_DATE - INTERVAL '30' DAY
+            GROUP BY ci.product_id, p.name
+            ORDER BY added_count DESC, last_added_at DESC
+            LIMIT 5
+            """.trimIndent()
+
+        val products = jdbcTemplate.query(sql, cartItemForMostWanted)
+        return products
+    }
+
+    fun addProductToStats(
+        productId: Long,
+        cartId: Long,
+    ): Long {
+        val now = Timestamp(System.currentTimeMillis())
+        val insert =
+            SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("CART_HISTORY")
+                .usingGeneratedKeyColumns("id")
+
+        val productMap =
+            mapOf(
+                "cart_id" to cartId,
+                "product_id" to productId,
+                "status" to "ADDED",
+                "created_at" to now,
+            )
+        return insert.executeAndReturnKey(productMap).toLong()
+    }
+}
