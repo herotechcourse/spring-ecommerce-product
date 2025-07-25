cbf0896ac473b4b051dac7aa035730fcfddd9982
Sanghun Park <po4865@gmail.com>
2025-07-25 09:46:38 +0200

    config: add JJWT as new dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,10 +26,14 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("io.jsonwebtoken:jjwt:0.9.1")
+
     runtimeOnly("com.h2database:h2")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("io.rest-assured:rest-assured:5.3.1")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
