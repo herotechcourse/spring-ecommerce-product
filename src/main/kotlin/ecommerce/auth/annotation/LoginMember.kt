a3605b7a123d4dc9c10ff096dace9f77a7e3460c
AzureAD\AnastasiiaLvutina <anasteishanakir@gmail.com>
2025-07-26 13:21:11 +0200

    style(LoginMember): remove comments

--- src/main/kotlin/ecommerce/auth/annotation/LoginMember.kt
+++ src/main/kotlin/ecommerce/auth/annotation/LoginMember.kt
@@ -3,8 +3,8 @@ package ecommerce.auth.annotation
 import kotlin.annotation.AnnotationRetention
 import kotlin.annotation.AnnotationTarget
 
-@Target(AnnotationTarget.VALUE_PARAMETER) //  аннотация @LoginMember может использоваться только для параметров функции
+@Target(AnnotationTarget.VALUE_PARAMETER)
 @Retention(
     AnnotationRetention.RUNTIME,
-) // аннотация @LoginMember будет доступна в байт-коде и её можно будет прочитать во время выполнения программы с помощью рефлексии
+)
 annotation class LoginMember
