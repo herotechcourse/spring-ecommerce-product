454488ffac04ba76d0394318a046b49c28e40b7f
pebencze <petra.bencze@deliveryhero.com>
2025-07-26 17:25:51 +0200

    refactor(LoginMemberArgumentResolver.kt): reuse AuthorizationExtractor in class

--- src/main/kotlin/ecommerce/annotation/LoginMemberArgumentResolver.kt
+++ src/main/kotlin/ecommerce/annotation/LoginMemberArgumentResolver.kt
@@ -1,15 +1,19 @@
 package ecommerce.annotation
 
 import ecommerce.exception.UnauthorizedException
+import ecommerce.infrastructure.AuthorizationExtractor
 import ecommerce.service.AuthService
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.core.MethodParameter
-import org.springframework.http.HttpHeaders
 import org.springframework.web.bind.support.WebDataBinderFactory
 import org.springframework.web.context.request.NativeWebRequest
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.method.support.ModelAndViewContainer
 
-class LoginMemberArgumentResolver(private val authService: AuthService) : HandlerMethodArgumentResolver {
+class LoginMemberArgumentResolver(
+    private val authService: AuthService,
+    private val authExtractor: AuthorizationExtractor
+) : HandlerMethodArgumentResolver {
     override fun supportsParameter(parameter: MethodParameter): Boolean {
         return parameter.hasParameterAnnotation(LoginMember::class.java)
     }
@@ -20,10 +24,12 @@ class LoginMemberArgumentResolver(private val authService: AuthService) : Handle
         webRequest: NativeWebRequest,
         binderFactory: WebDataBinderFactory?
     ): Any {
-        val token = webRequest
-            .getHeader(HttpHeaders.AUTHORIZATION)
-            ?.removePrefix("Bearer ")
+        val request = webRequest.getNativeRequest(HttpServletRequest::class.java)
             ?: throw UnauthorizedException()
+
+        val token = authExtractor.extract(request)
+        if (token.isBlank()) throw UnauthorizedException()
+
         return authService.findMemberByToken(token)
     }
 }
