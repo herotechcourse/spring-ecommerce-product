c77744cd023ba484a4894575c973ed9910f3c300
Sanghun Park <po4865@gmail.com>
2025-07-26 20:16:16 +0200

    fix(GlobalExceptionHandler): edit exception handlers not to provide error message to client

--- src/main/kotlin/ecommerce/GlobalExceptionHandler.kt
+++ src/main/kotlin/ecommerce/GlobalExceptionHandler.kt
@@ -22,18 +22,14 @@ class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(DataAccessException::class)
-    fun handleDataAccessException(e: Exception): ResponseEntity<Map<String, Any>> {
-        val error = mapOf("message" to e.message)
-        val errorBody = mapOf("errors" to error)
-        println("DataAccessException occurred: $errorBody")
-        return ResponseEntity.internalServerError().body(errorBody)
+    fun handleDataAccessException(e: Exception): ResponseEntity<Void> {
+        println("DataAccessException occurred: " + e.message)
+        return ResponseEntity.internalServerError().build()
     }
 
     @ExceptionHandler(IllegalStateException::class)
-    fun handlerIllegalStateException(e: Exception): ResponseEntity<Map<String, Any>> {
-        val error = mapOf("message" to e.message)
-        val errorBody = mapOf("errors" to error)
-        println("IllegalStateException occurred: $errorBody")
-        return ResponseEntity.internalServerError().body(errorBody)
+    fun handlerIllegalStateException(e: Exception): ResponseEntity<Void> {
+        println("IllegalStateException occurred: " + e.message)
+        return ResponseEntity.internalServerError().build()
     }
 }
