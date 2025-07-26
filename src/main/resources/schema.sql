7d73d87fa7e7c78ebfaf71aa366ef418ab180a68
AzureAD\ApoorvaSomanahallyNa <apoorva1326@gmail.com>
2025-07-26 17:55:54 +0200

    feat(schema): add member schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,10 @@ CREATE TABLE IF NOT EXISTS products (
     img      VARCHAR(120) NOT NULL,
     quantity INTEGER      NOT NULL
     );
+
+CREATE TABLE IF NOT EXISTS members (
+    id          VARCHAR(40)     PRIMARY KEY,
+    email       VARCHAR(255)    UNIQUE NOT NULL,
+    password    VARCHAR(255)    NOT NULL,
+    role        VARCHAR(255)    NOT NULL
+   );
