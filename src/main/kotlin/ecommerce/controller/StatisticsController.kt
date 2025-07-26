b85020a52ae0e8c28186300d866a46ac631dd884
pebencze <petra.bencze@deliveryhero.com>
2025-07-26 20:10:46 +0200

    feat( StatisticsController.kt ): add blueprint for controller

---
+++ src/main/kotlin/ecommerce/controller/StatisticsController.kt
@@ -0,0 +1,24 @@
+package ecommerce.controller
+
+import ecommerce.dto.MemberStatsDto
+import ecommerce.dto.ProductStatsDto
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/admin/statistics")
+class StatisticsController(private val statService: StatService){
+    @GetMapping("/topProducts")
+    fun topProducts(): ResponseEntity<ProductStatsDto> {
+        val stats = statService.topProducts()
+        return ResponseEntity.ok(stats)
+    }
+
+    @GetMapping("/activeMembers")
+    fun activeMembers(): ResponseEntity<List<MemberStatsDto>> {
+        val stats = statService.activeMembers()
+        return ResponseEntity.ok(stats)
+    }
+}
