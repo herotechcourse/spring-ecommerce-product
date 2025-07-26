c16117226f22831b98e8a36d4eb4397f233818a6
Sanghun Park <po4865@gmail.com>
2025-07-26 20:22:31 +0200

    refactor(ProductService): refine logic for 'insert()', 'update()', and 'checkProductNameExists()'

--- src/main/kotlin/ecommerce/service/ProductService.kt
+++ src/main/kotlin/ecommerce/service/ProductService.kt
@@ -2,31 +2,19 @@ package ecommerce.service
 
 import ecommerce.dao.JdbcProductDAO
 import ecommerce.dto.ProductForm
+import ecommerce.exception.InternalServerErrorException
 import ecommerce.exception.ProductNameAlreadyExistsException
 import ecommerce.model.Product
-import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
-import java.net.URI
 
 @Service
 class ProductService(private val jdbcProductDao: JdbcProductDAO) {
-    fun insert(form: ProductForm): ResponseEntity<Product> {
+    fun insert(form: ProductForm): Product {
         checkProductNameExists(form.name)
         val product = ProductForm.toProduct(form)
         val id = jdbcProductDao.insert(product)
-        val insertedProduct = jdbcProductDao.findById(id)
-        val uri = URI.create("/api/products/$id")
-        return ResponseEntity.created(uri).body(insertedProduct)
-    }
-
-    private fun checkProductNameExists(name: String): Boolean {
-        when (jdbcProductDao.existsByName(name)) {
-            true -> {
-                val message = "Product with name '$name' already exists."
-                throw ProductNameAlreadyExistsException(message)
-            }
-            false -> return true
-        }
+        return jdbcProductDao.findById(id)
+            ?: throw InternalServerErrorException("ProductService.insert() - Product with ID $id not found")
     }
 
     fun findAll(): List<Product> = jdbcProductDao.findAll()
@@ -37,10 +25,30 @@ class ProductService(private val jdbcProductDao: JdbcProductDAO) {
         form: ProductForm,
         id: Long,
     ): Int {
-        checkProductNameExists(form.name)
+        val originalProduct =
+            jdbcProductDao.findById(id)
+                ?: throw InternalServerErrorException("ProductService.update() - Product with ID $id not found")
+        val originalName = originalProduct.name
+        checkProductNameExists(form.name, originalName)
         val product = ProductForm.toEntity(form, id)
-        return jdbcProductDao.update(product)
+        val numberOfAffectedRow = jdbcProductDao.update(product)
+        return numberOfAffectedRow
     }
 
-    fun delete(id: Long): Int = jdbcProductDao.delete(id)
+    fun delete(id: Long): Int {
+        val numberOfAffectedRow = jdbcProductDao.delete(id)
+        return numberOfAffectedRow
+    }
+
+    private fun checkProductNameExists(
+        name: String,
+        originalName: String? = null,
+    ) {
+        if (originalName != null && name == originalName) {
+            return
+        } else if (jdbcProductDao.existsByName(name)) {
+            val message = "Product with name '$name' already exists."
+            throw ProductNameAlreadyExistsException(message)
+        }
+    }
 }
--- src/test/kotlin/ecommerce/service/ProductServiceTest.kt
+++ src/test/kotlin/ecommerce/service/ProductServiceTest.kt
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.HttpStatus
 import org.springframework.jdbc.core.JdbcTemplate
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -47,15 +46,41 @@ class ProductServiceTest {
     }
 
     @Test
-    fun `insert() - should return 400 when name of product already exists`() {
+    fun `insert() - should throw an exception when name of product already exists`() {
         val productForm = ProductForm(name = "Iron Man", price = 1.5, imageUrl = "https://www.product.com/image/1")
         assertThrows<ProductNameAlreadyExistsException> { productService.insert(productForm) }
     }
 
     @Test
-    fun `insert() - should return 201 when name of product not exists`() {
+    fun `insert() - should insert and return the product when name of product not exists`() {
         val productForm = ProductForm(name = "Iron Body", price = 1.5, imageUrl = "https://www.product.com/image/1")
-        val response = productService.insert(productForm)
-        assertThat(response.statusCode.value()).isEqualTo(HttpStatus.CREATED.value())
+        val result = productService.insert(productForm)
+        assertThat(result.id).isNotNull()
+        assertThat(result.name).isEqualTo(productForm.name)
+        assertThat(result.price).isEqualTo(productForm.price)
+        assertThat(result.imageUrl).isEqualTo(productForm.imageUrl)
+    }
+
+    @Test
+    fun `update() - should throw an exception when name of product already exists`() {
+        val id = 2L
+        val productForm = ProductForm(name = "Iron Man", price = 10.5, imageUrl = "https://www.product.com/image/1")
+        assertThrows<ProductNameAlreadyExistsException> { productService.update(productForm, id) }
+    }
+
+    @Test
+    fun `update() - should update the product when original product and new product have same name`() {
+        val id = 1L
+        val productForm = ProductForm(name = "Iron Man", price = 10.5, imageUrl = "https://www.product.com/image/1")
+        val numberOfAffectedRow = productService.update(productForm, id)
+        assertThat(numberOfAffectedRow).isEqualTo(1)
+    }
+
+    @Test
+    fun `update() - should update the product when name of new product does not exists`() {
+        val id = 1L
+        val productForm = ProductForm(name = "Stone Body", price = 1.5, imageUrl = "https://www.product.com/image/1")
+        val numberOfAffectedRow = productService.update(productForm, id)
+        assertThat(numberOfAffectedRow).isEqualTo(1)
     }
 }
