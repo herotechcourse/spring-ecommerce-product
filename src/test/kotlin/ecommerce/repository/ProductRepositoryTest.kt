49efe906e1334ff66d06de37be7977ae6a35d304
knopgm <knop.gm@gmail.com>
2025-07-26 13:33:11 +0200

    chore (ProductRepositoryTest): remove unused import

--- src/test/kotlin/ecommerce/repository/ProductRepositoryTest.kt
+++ src/test/kotlin/ecommerce/repository/ProductRepositoryTest.kt
@@ -1,7 +1,6 @@
 package ecommerce.repository
 
 import ecommerce.model.Product
-import ecommerce.repository.ProductStore
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
