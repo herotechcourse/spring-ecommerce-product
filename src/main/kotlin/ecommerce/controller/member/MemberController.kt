b7db13dbeb7f137804009a1fe73b7aaa93aebaaf
AzureAD\ApoorvaSomanahallyNa <apoorva1326@gmail.com>
2025-07-26 17:48:09 +0200

    feat(memberController): add member controller to access products endpoint

--- src/main/kotlin/ecommerce/controller/member/MemberController.kt
+++ src/main/kotlin/ecommerce/controller/member/MemberController.kt
@@ -1,4 +1,19 @@
 package ecommerce.controller.member
 
+import ecommerce.dto.product.ProductResponse
+import ecommerce.service.ProductService
+import ecommerce.utils.toResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/member")
 class MemberController {
+    @GetMapping("/products")
+    fun getAllProducts(productService: ProductService): ResponseEntity<List<ProductResponse>> {
+        val products = productService.getAllProducts().map { it.toResponse() }
+        return ResponseEntity.ok(products)
+    }
 }
