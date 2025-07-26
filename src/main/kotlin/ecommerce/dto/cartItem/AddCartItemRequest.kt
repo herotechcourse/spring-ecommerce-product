27b3f662888d8967b0811a9feff38d42f5d93c0a
AzureAD\ApoorvaSomanahallyNa <apoorva1326@gmail.com>
2025-07-26 22:06:59 +0200

    feat(cat dto): add Cart Item response and request dto

---
+++ src/main/kotlin/ecommerce/dto/cartItem/AddCartItemRequest.kt
@@ -0,0 +1,13 @@
+package ecommerce.dto.cartItem
+
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotNull
+
+data class AddCartItemRequest(
+    @field: NotNull(message = "Product ID cannot be null")
+    @field: Min(1, message = "Product ID must be positive")
+    val productId: Long,
+    @field: NotNull(message="Quantity cannot be null")
+    @field: Min(1, message="Quantity must be atleast 1")
+    val quantity: Int
+)
