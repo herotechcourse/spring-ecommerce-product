1ffd7abc7fb1d586866abdb639b3b91d43299034
pebencze <petra.bencze@deliveryhero.com>
2025-07-26 13:39:22 +0200

    refactor(CartService): use new repo logic inside service

--- src/main/kotlin/ecommerce/service/CartService.kt
+++ src/main/kotlin/ecommerce/service/CartService.kt
@@ -1,9 +1,9 @@
 package ecommerce.service
 
+import ecommerce.dto.AllCartItemsDto
 import ecommerce.dto.CartItemDto
 import ecommerce.dto.CartItemRequest
 import ecommerce.exception.NotFoundException
-import ecommerce.model.Product
 import ecommerce.repository.CartRepository
 import ecommerce.repository.ProductRepository
 import org.springframework.stereotype.Service
@@ -13,35 +13,26 @@ class CartService(
     private val cartRepository: CartRepository,
     private val productRepository: ProductRepository,
 ) {
-    fun getAllItems(memberId: Long): CartItemDto {
-        val cartId = cartRepository.findOrCreateCartId(memberId) ?: throw CartCreationException()
-        val rawItems = cartRepository.showAllItemsInCart(cartId)
-        val items = mutableListOf<Product>()
-        rawItems.map { rawItem ->
-             val product = productRepository.findById(rawItem.productId)
-                ?: throw NotFoundException("Product with ${rawItem.productId} not found")
-            items.add(product)
-        }
-        return CartItemDto(cartId = cartId, quantity = rawItem.quantity, items = items)
-        // Can in `showAllItemsInCart` something go wrong? How can I prove it?
-    }
-
-    fun addItems(memberId: Long, request: CartItemRequest): CartItemDto {
+    fun getAllItems(memberId: Long): AllCartItemsDto {
         val cartId = cartRepository.findOrCreateCartId(memberId)
-            ?: throw CartCreationException()
-        repeat(request.quantity) {
-            if (!cartRepository.addProductToCart(request.productId, cartId)) { throw ItemManipulationException("Product could not be added.") }
-        }
-        val quantity = cartRepository.quantityInCart(request.productId, cartId)
-        val product = productRepository.findById(request.productId) ?: throw NotFoundException("Product with ${request.productId} not found")
-        return CartItemDto(cartId, quantity, listOf(product))
+        val items = cartRepository.showAllItemsInCart(cartId)
+        return AllCartItemsDto(cartId, items)
     }
 
-    fun deleteItems(memberId: Long, request: CartItemRequest) {
-        val cartId = cartRepository.findOrCreateCartId(memberId) ?: throw CartCreationException()
-        repeat(request.quantity) {
-            if (!cartRepository.addProductToCart(request.productId, cartId)) { throw ItemManipulationException("Product could not be deleted.") }
+    fun addItem(memberId: Long, request: CartItemRequest): CartItemDto {
+        val cartId = cartRepository.findOrCreateCartId(memberId)
+        if (!productRepository.existsById(request.productId)) {
+            throw NotFoundException("Product with ${request.productId} not found")
         }
+        return cartRepository.addItemToCart(request.productId, request.quantity, cartId)
+    }
+
+    fun deleteItem(memberId: Long, request: CartItemRequest): CartItemDto? {
+        val cartId = cartRepository.findOrCreateCartId(memberId)
+        if (!productRepository.existsById(request.productId)) {
+            throw NotFoundException("Product with ${request.productId} not found")
+        }
+        return cartRepository.removeItemFromCart(request.productId, request.quantity, cartId)
     }
 
 }
