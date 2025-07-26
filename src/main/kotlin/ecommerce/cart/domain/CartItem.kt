6692ce10565c984f8b9b10c072035f017c8e44a2
AzureAD\AnastasiiaLvutina <anasteishanakir@gmail.com>
2025-07-26 11:51:16 +0200

    feat(CartItem): add new property - val addedAt: LocalDateTime? = LocalDateTime.now()

--- src/main/kotlin/ecommerce/cart/domain/CartItem.kt
+++ src/main/kotlin/ecommerce/cart/domain/CartItem.kt
@@ -1,7 +1,10 @@
 package ecommerce.cart.domain
 
+import java.time.LocalDateTime
+
 data class CartItem(
     var id: Long? = null,
     val memberId: Long,
     val productId: Long,
+    val addedAt: LocalDateTime? = LocalDateTime.now(),
 )
