947dce732e9c3959a71d43bc04587efaf146566b
farhanaahmed <farhana.ahmed.fima@gmail.com>
2025-07-27 16:14:41 +0200

    feat: add cart table to schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,11 +4,22 @@ CREATE TABLE IF NOT EXISTS products (
     price DOUBLE CHECK (price >= 0),
     image_url VARCHAR(255)
 );
-CREATE TABLE IF NOT EXISTS MEMBERS
+CREATE TABLE IF NOT EXISTS members
 (
     id       INT NOT NULL AUTO_INCREMENT,
     email    VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     role     VARCHAR(50)  NOT NULL,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS carts (
+    id INT  PRIMARY KEY AUTO_INCREMENT,
+    member_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT DEFAULT 1,
+
+    UNIQUE KEY unique_cart_item (member_id, product_id),
+    FOREIGN KEY (member_id) REFERENCES members(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
 );
