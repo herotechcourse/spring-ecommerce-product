334771dcee4cdbd923f1ca6c1a17b2d7bb33f0a0
knopgm <knop.gm@gmail.com>
2025-07-27 13:06:52 +0200

    refactor (MemberDTO): add a role param to differentiate users from admin

--- src/main/kotlin/ecommerce/dto/MemberDTO.kt
+++ src/main/kotlin/ecommerce/dto/MemberDTO.kt
@@ -1,15 +1,18 @@
 package ecommerce.dto
 
 import ecommerce.model.Member
+import ecommerce.model.Role
 
 data class MemberDTO(
     var email: String,
     var password: String,
+    var role: Role = Role.USER
 ) {
     fun toEntity(): Member {
         return Member(
             email = this.email,
-            password = this.password
+            password = this.password,
+            role = this.role
         )
     }
 }
