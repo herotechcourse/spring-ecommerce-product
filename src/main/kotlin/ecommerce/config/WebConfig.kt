643cd2fb9ccb59a918fba9cdf03883f3dc52afda
knopgm <knop.gm@gmail.com>
2025-07-27 17:40:59 +0200

    feat (WebConfig, LoginMemberArgumentResolver): implement a web config and argument resolver to check if the login of each request has the authorization for to use it.

---
+++ src/main/kotlin/ecommerce/config/WebConfig.kt
@@ -0,0 +1,16 @@
+package ecommerce.config
+
+import ecommerce.resolver.LoginMemberArgumentResolver
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig(
+    private val loginMemberArgumentResolver: LoginMemberArgumentResolver
+) : WebMvcConfigurer {
+
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
+        resolvers.add(loginMemberArgumentResolver)
+    }
+}
---
+++ src/main/kotlin/ecommerce/resolver/LoginMemberArgumentResolver.kt
@@ -0,0 +1,39 @@
+package ecommerce.resolver
+
+import ecommerce.model.Member
+import ecommerce.service.AuthService
+import ecommerce.service.MemberService
+import org.springframework.core.MethodParameter
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.support.WebDataBinderFactory
+import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.method.support.ModelAndViewContainer
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class LoginMember
+
+@Component
+class LoginMemberArgumentResolver(
+    private val authService: AuthService,
+    private val memberService: MemberService
+) : HandlerMethodArgumentResolver {
+
+    override fun supportsParameter(parameter: MethodParameter): Boolean {
+        return parameter.hasParameterAnnotation(LoginMember::class.java)
+    }
+
+    override fun resolveArgument(
+        parameter: MethodParameter,
+        mavContainer: ModelAndViewContainer?,
+        webRequest: NativeWebRequest,
+        binderFactory: WebDataBinderFactory?
+    ): Member {
+        val userEmail = authService.extractAndValidateToken(webRequest.getHeader("Authorization") ?: "")
+        val member = memberService.findByEmail(userEmail) ?: throw UnauthorizedException()
+        return member
+    }
+}
+
+class UnauthorizedException: RuntimeException("Unauthorized")
