597e7357ad829182ff49f697b8b233f766f2327c
Sanghun Park <po4865@gmail.com>
2025-07-27 23:22:43 +0200

    chore(schema): define a new table 'cart_item'

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -18,3 +18,17 @@ CREATE TABLE member
     password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
+
+-- DROP TABLE cart_item CASCADE;
+
+CREATE TABLE cart_item
+(
+    id         LONG NOT NULL AUTO_INCREMENT,
+    member_id  LONG NOT NULL,
+    product_id LONG NOT NULL,
+    quantity   INT DEFAULT 1,
+    PRIMARY KEY (id),
+
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (product_id) REFERENCES product (id)
+);
