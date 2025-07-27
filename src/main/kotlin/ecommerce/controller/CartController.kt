5e51b77299f7efb29ef3c567be9c33ffb61c1c54
knopgm <knop.gm@gmail.com>
2025-07-28 00:12:52 +0200

    feat (CartController): implement a cart controller to deal with all basic request commands

---
+++ src/main/kotlin/ecommerce/controller/CartController.kt
@@ -0,0 +1,44 @@
+package ecommerce.controller
+
+import ecommerce.model.CartItem
+import ecommerce.model.Member
+import ecommerce.resolver.LoginMember
+import ecommerce.service.CartService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/api/cart")
+@RestController
+class CartController(private val cartService: CartService) {
+
+    @PostMapping("add/{productId}")
+    fun addToCart(
+        @LoginMember member: Member,
+        @PathVariable("productId") productId: Long
+    ): ResponseEntity<CartItem> {
+        val saved = cartService.addProductToCart(member.id, productId, 1)
+        return ResponseEntity.ok(saved)
+    }
+
+    @GetMapping
+    fun getCartItems(@LoginMember member: Member): ResponseEntity<List<CartItem>> {
+        val items = cartService.getCartItemsForMember(member.id)
+        if (items.isEmpty()) return ResponseEntity.noContent().build()
+        return ResponseEntity.ok(items)
+    }
+
+    @DeleteMapping("/{productId}")
+    fun deleteCartItem(
+        @LoginMember member: Member,
+        @PathVariable productId: Long
+    ): ResponseEntity<Void> {
+        val deleted = cartService.removeItemFromCart(member.id, productId)
+        return if (deleted != null) ResponseEntity.noContent().build()
+        else ResponseEntity.notFound().build()
+    }
+}
