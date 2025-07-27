8e94ffc3921ab17acaca32053e36e80ff35c7859
farhanaahmed <farhana.ahmed.fima@gmail.com>
2025-07-27 12:27:20 +0200

    feat: add JWT provider

---
+++ src/main/kotlin/ecommerce/infrastructure/JWTProvider.kt
@@ -0,0 +1,45 @@
+package ecommerce.infrastructure
+
+import io.jsonwebtoken.JwtException
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.util.Date
+
+@Component
+class JWTProvider {
+    @Value("\${security.jwt.token.secret-key}")
+    private lateinit var secretKey: String
+
+    @Value("\${security.jwt.token.expire-length}")
+    private var validityInMilliseconds: Long = 0
+
+    fun createToken(payload: String): String {
+        val claims = Jwts.claims().setSubject(payload)
+        val now = Date()
+        val validity = Date(now.time + validityInMilliseconds)
+
+        return Jwts.builder()
+            .setClaims(claims)
+            .setIssuedAt(now)
+            .setExpiration(validity)
+            .signWith(SignatureAlgorithm.HS256, secretKey)
+            .compact()
+    }
+
+    fun getPayload(token: String): String {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).body.subject
+    }
+
+    fun validateToken(token: String): Boolean {
+        return try {
+            val claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token)
+            !claims.body.expiration.before(Date())
+        } catch (e: JwtException) {
+            false
+        } catch (e: IllegalArgumentException) {
+            false
+        }
+    }
+}
