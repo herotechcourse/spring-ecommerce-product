34560d3b052bcf0a096d464d5c640cf0dbec62ee
Mina Kim <mina.kim@deliveryhero.com>
2025-07-28 23:43:10 +0200

    chore: cleanup code

--- src/main/kotlin/ecommerce/dto/MemberRequest.kt
+++ src/main/kotlin/ecommerce/dto/MemberRequest.kt
@@ -8,10 +8,6 @@ class MemberRequest(
     @field:Email(message = "Invalid email format")
     @field:NotBlank(message = "Email must not be blank")
     val email: String,
-    //    @field:Pattern(
-//        regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#\$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,20}\$",
-//        message = "Password must contain letters, digits, and special characters"
-//    )
     @field:Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
     val password: String,
 )
