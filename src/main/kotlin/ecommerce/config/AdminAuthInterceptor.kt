8ae62e18325f3a844cb982335b68180e4c93c2b3
pebencze <petra.bencze@deliveryhero.com>
2025-07-28 11:37:10 +0200

    refactor(AuthInterceptor): return 403 instead of 401

--- src/main/kotlin/ecommerce/config/AdminAuthInterceptor.kt
+++ src/main/kotlin/ecommerce/config/AdminAuthInterceptor.kt
@@ -1,13 +1,14 @@
 package ecommerce.config
 
-import ecommerce.exception.UnauthorizedException
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.web.servlet.HandlerInterceptor
 import ecommerce.infrastructure.AuthorizationExtractor
 import ecommerce.service.AuthService
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
 
-
+@Component
 class AdminAuthInterceptor(
     private val authExtractor: AuthorizationExtractor,
     private val authService: AuthService
@@ -18,9 +19,17 @@ class AdminAuthInterceptor(
         handler: Any
     ): Boolean {
         val token = authExtractor.extract(request)
-        if (token.isEmpty()) throw UnauthorizedException()
-        val admin = authService.findMember(token)
-        if (admin.role != "admin") throw UnauthorizedException()
+        if (token.isEmpty()){
+            response.status = HttpStatus.FORBIDDEN.value()
+            return false
+        }
+
+        val admin = authService.findMemberByToken(token)
+        if (admin.role != "admin")
+        {
+            response.status = HttpStatus.FORBIDDEN.value()
+            return false
+        }
         return true
     }
 }
