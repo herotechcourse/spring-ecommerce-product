4f839cdb7a2b1484cd08528b516cb9718315a0d2
pebencze <petra.bencze@deliveryhero.com>
2025-07-28 12:50:37 +0200

    feat( StatisticsController.kt ): add @Admin parameter

--- src/main/kotlin/ecommerce/controller/StatisticsController.kt
+++ src/main/kotlin/ecommerce/controller/StatisticsController.kt
@@ -1,5 +1,7 @@
 package ecommerce.controller
 
+import ecommerce.annotation.Admin
+import ecommerce.dto.MemberDto
 import ecommerce.dto.MemberStatsDto
 import ecommerce.dto.ProductStatsDto
 import ecommerce.service.StatService
@@ -12,13 +14,13 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/admin/statistics")
 class StatisticsController(private val statService: StatService){
     @GetMapping("/top-products")
-    fun topProducts(): ResponseEntity<List<ProductStatsDto>> {
+    fun topProducts(@Admin admin: MemberDto): ResponseEntity<List<ProductStatsDto>> {
         val stats = statService.getTop5ProductsInThePast30Days()
         return ResponseEntity.ok(stats)
     }
 
-    @GetMapping("/active-members")
-    fun activeMembers(): ResponseEntity<List<MemberStatsDto>> {
+    @GetMapping("/admin/active-members")
+    fun activeMembers(@Admin admin: MemberDto): ResponseEntity<List<MemberStatsDto>> {
         val stats = statService.getActiveMembersInThePast7Days()
         return ResponseEntity.ok(stats)
     }
