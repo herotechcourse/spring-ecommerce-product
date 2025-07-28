514f512d389a2fc1ab291aeefd206cd003d5bda7
AzureAD\AnastasiiaLvutina <anasteishanakir@gmail.com>
2025-07-28 10:04:23 +0200

    refactor(AdminController): simplify AdminController by removing ResponseEntity - remove ResponseEntity from getTopProducts and getRecentMembers

--- src/main/kotlin/ecommerce/admin/controller/AdminController.kt
+++ src/main/kotlin/ecommerce/admin/controller/AdminController.kt
@@ -5,7 +5,6 @@ import ecommerce.admin.dto.TopProductResponse
 import ecommerce.admin.service.AdminService
 import ecommerce.auth.annotation.LoginMember
 import ecommerce.member.domain.Member
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -16,14 +15,14 @@ class AdminController(private val adminService: AdminService) {
     @GetMapping("/top-products")
     fun getTopProducts(
         @LoginMember member: Member,
-    ): ResponseEntity<List<TopProductResponse>> {
-        return ResponseEntity.ok(adminService.getTopProducts())
+    ): List<TopProductResponse> {
+        return adminService.getTopProducts()
     }
 
     @GetMapping("/recent-members")
     fun getRecentMembers(
         @LoginMember member: Member,
-    ): ResponseEntity<List<RecentMemberResponse>> {
-        return ResponseEntity.ok(adminService.getRecentMembers())
+    ): List<RecentMemberResponse> {
+        return adminService.getRecentMembers()
     }
 }
