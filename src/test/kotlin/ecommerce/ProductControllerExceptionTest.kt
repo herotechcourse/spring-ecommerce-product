f4916da39ff49fadf5c693cab4f154260fa188f2
pebencze <petra.bencze@deliveryhero.com>
2025-07-28 15:07:59 +0200

    refactor( ProductControllerExceptionTest.kt ): extract duplicate code

--- src/test/kotlin/ecommerce/ProductControllerExceptionTest.kt
+++ src/test/kotlin/ecommerce/ProductControllerExceptionTest.kt
@@ -3,6 +3,8 @@ package ecommerce
 import ecommerce.model.Product
 import io.restassured.RestAssured
 import io.restassured.http.ContentType
+import io.restassured.response.ExtractableResponse
+import io.restassured.response.Response
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
@@ -12,20 +14,20 @@ import org.springframework.test.annotation.DirtiesContext
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ProductControllerExceptionTest {
+
+    private fun makeRequestToProducts(product: Product): ExtractableResponse<Response> = RestAssured
+            .given().log().all().body(product)
+            .contentType(ContentType.JSON)
+            .`when`().post("/products")
+            .then().log().all().extract()
+
     @Test
     fun handleExceptionUsingExceptionHandler() {
-        val response =
-            RestAssured
-                .given().log().all().body(
-                    Product(
-                        name = "colaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-                        price = 0.0,
-                        imageUrl = "abchttps://cola.jpg",
-                    ),
-                )
-                .contentType(ContentType.JSON)
-                .`when`().post("/products")
-                .then().log().all().extract()
+        val response = makeRequestToProducts(Product(
+            name = "colaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+            price = 0.0,
+            imageUrl = "abchttps://cola.jpg",)
+        )
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value())
         assertThat(response.body().asString()).contains("Product name cannot exceed 15 characters")
@@ -33,19 +35,8 @@ class ProductControllerExceptionTest {
 
     @Test
     fun sameNameException() {
-        val response =
-            RestAssured
-                .given().log().all().body(Product(name = "cola", price = 4.5, imageUrl = "https://cola.jpg"))
-                .contentType(ContentType.JSON)
-                .`when`().post("/products")
-                .then().log().all().extract()
-
-        val sameNameResponse =
-            RestAssured
-                .given().log().all().body(Product(name = "cola", price = 4.5, imageUrl = "https://cola.jpg"))
-                .contentType(ContentType.JSON)
-                .`when`().post("/products")
-                .then().log().all().extract()
+        val response = makeRequestToProducts(Product(name = "cola", price = 4.5, imageUrl = "https://cola.jpg"))
+        val sameNameResponse = makeRequestToProducts(Product(name = "cola", price = 4.5, imageUrl = "https://cola.jpg"))
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value())
         assertThat(sameNameResponse.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value())
