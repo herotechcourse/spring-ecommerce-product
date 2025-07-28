1f1314296b2f5987b3bed7a504c945d2cb3f0f69
AzureAD\AnastasiiaLvutina <anasteishanakir@gmail.com>
2025-07-28 11:38:42 +0200

    style(LoginMember): standardize LoginMember annotation format - remove trailing comma in @Retention - format @Retention in single line for consistency

--- src/main/kotlin/ecommerce/auth/annotation/LoginMember.kt
+++ src/main/kotlin/ecommerce/auth/annotation/LoginMember.kt
@@ -1,10 +1,5 @@
 package ecommerce.auth.annotation
 
-import kotlin.annotation.AnnotationRetention
-import kotlin.annotation.AnnotationTarget
-
 @Target(AnnotationTarget.VALUE_PARAMETER)
-@Retention(
-    AnnotationRetention.RUNTIME,
-)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class LoginMember
