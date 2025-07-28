c3da4589e42405f7761d1ec6e534adc64401bc7b
pebencze <petra.bencze@deliveryhero.com>
2025-07-28 12:44:51 +0200

    feat( Admin.kt, AdminArgumentResolver.kt ): add custom annotation @Admin for value parameters to verify members' role

---
+++ src/main/kotlin/ecommerce/annotation/AdminArgumentResolver.kt
@@ -0,0 +1,47 @@
+package ecommerce.annotation
+
+import ecommerce.dto.MemberDto
+import ecommerce.exception.ForbiddenException
+import ecommerce.exception.NotFoundException
+import ecommerce.exception.UnauthorizedException
+import ecommerce.service.AuthService
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.core.MethodParameter
+import org.springframework.web.bind.support.WebDataBinderFactory
+import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.method.support.ModelAndViewContainer
+
+
+class AdminArgumentResolver(
+    private val authService: AuthService,
+) : HandlerMethodArgumentResolver {
+    override fun supportsParameter(parameter: MethodParameter): Boolean {
+        return parameter.hasParameterAnnotation(Admin::class.java)
+    }
+
+    override fun resolveArgument(
+        parameter: MethodParameter,
+        mavContainer: ModelAndViewContainer?,
+        webRequest: NativeWebRequest,
+        binderFactory: WebDataBinderFactory?,
+    ): Any {
+        lateinit var member: MemberDto
+        val request =
+            webRequest.getNativeRequest(HttpServletRequest::class.java)
+                ?: throw UnauthorizedException()
+
+        val email = request.getAttribute("email") ?: throw UnauthorizedException()
+        try {
+            member = authService.findMember(email.toString())
+        } catch (e: NotFoundException) {
+            throw UnauthorizedException()
+        }
+
+        if (member.role != "admin") {
+            throw ForbiddenException()
+        }
+
+        return member
+    }
+}
