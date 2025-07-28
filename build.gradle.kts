78e7aea3154c151d42225ffe9a30c3052167ca3b
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-07-28 12:23:32 +0200

    refactor: hide secrets

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    implementation("io.github.cdimascio:dotenv-java:3.0.0")
 
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
