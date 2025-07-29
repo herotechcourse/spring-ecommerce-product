3a64e8568aee265263f67b8609ba7de91c2fed97
Jin Park <jieun.park.e@deliveryhero.com>
2025-07-29 10:32:03 +0200

    remove(CarRequest.kt): unused class

--- src/main/kotlin/ecommerce/dto/cart/CartRequest.kt
+++
@@ -1,8 +0,0 @@
-package ecommerce.dto.cart
-
-import jakarta.validation.constraints.PositiveOrZero
-
-data class CartRequest(
-    @field:PositiveOrZero(message = "Quantity can not be zero")
-    val quantity: Int,
-)
