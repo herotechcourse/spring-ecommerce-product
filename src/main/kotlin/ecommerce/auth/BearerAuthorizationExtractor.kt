9cd5cc9d82ead9aec2a10fe1fb15944f66005816
Sanghun Park <po4865@gmail.com>
2025-07-29 17:23:29 +0200

    chore(auth): remove a 'var' notation

--- src/main/kotlin/ecommerce/auth/BearerAuthorizationExtractor.kt
+++ src/main/kotlin/ecommerce/auth/BearerAuthorizationExtractor.kt
@@ -8,11 +8,11 @@ class BearerAuthorizationExtractor : AuthorizationExtractor<String> {
         while (headers.hasMoreElements()) {
             val value = headers.nextElement()
             if (value.lowercase().startsWith(BEARER_TYPE.lowercase())) {
-                var authHeaderValue = value.substring(BEARER_TYPE.length).trim()
+                val authHeaderValue = value.substring(BEARER_TYPE.length).trim()
                 request.setAttribute(ACCESS_TOKEN_TYPE, value.substring(0, BEARER_TYPE.length).trim())
                 val commaIndex = authHeaderValue.indexOf(',')
                 if (commaIndex > 0) {
-                    authHeaderValue = authHeaderValue.substring(0, commaIndex)
+                    return authHeaderValue.substring(0, commaIndex)
                 }
                 return authHeaderValue
             }
