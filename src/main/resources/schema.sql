b517268ff5168ba0112e58351b9c081c7cd22e4d
knopgm <knop.gm@gmail.com>
2025-08-02 22:13:55 +0200

    feat (schema.sql): add timestamp to the cart table and name to the members table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,7 +11,8 @@ CREATE TABLE members
     id       BIGINT PRIMARY KEY AUTO_INCREMENT,
     email    VARCHAR(320),
     password VARCHAR(8),
-    role     VARCHAR(8)
+    role     VARCHAR(8),
+    name     VARCHAR(50)
 );
 
 CREATE TABLE cart
@@ -19,5 +20,7 @@ CREATE TABLE cart
     id         BIGINT AUTO_INCREMENT PRIMARY KEY,
     member_id  BIGINT NOT NULL,
     product_id BIGINT NOT NULL,
-    quantity   INT    NOT NULL
+    quantity   INT    NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP
 );
