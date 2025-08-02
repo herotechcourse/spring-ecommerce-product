58dcbadb2df163707ff86c4e97452474c1770dfc
Mina Kim <mina.kim@deliveryhero.com>
2025-08-02 13:16:40 +0200

    refactor(domain): add basic require condition to NewCartItem and NewProduct

--- src/main/kotlin/ecommerce/domain/NewProduct.kt
+++ src/main/kotlin/ecommerce/domain/NewProduct.kt
@@ -1,5 +1,8 @@
 package ecommerce.domain
 
+import ecommerce.constants.ConstantsProduct.Validation.IMAGE_URL_MAX_LENGTH
+import ecommerce.constants.ConstantsProduct.Validation.NAME_MAX_LENGTH
+import ecommerce.constants.ConstantsProduct.Validation.PRICE_DECIMAL_SCALE
 import java.math.BigDecimal
 
 class NewProduct(
@@ -8,6 +11,9 @@ class NewProduct(
     val imageUrl: String,
 ) {
     init {
-        // TODO: logic
+        require(name.isNotBlank() && name.length in 2..NAME_MAX_LENGTH)
+        require(name.matches(Regex("^[a-zA-Z0-9\\s()\\[\\]+\\-&/_]+$")))
+        require(price > BigDecimal.ZERO && price.scale() == PRICE_DECIMAL_SCALE)
+        require(imageUrl.isNotBlank() && imageUrl.length in 1..IMAGE_URL_MAX_LENGTH)
     }
 }
