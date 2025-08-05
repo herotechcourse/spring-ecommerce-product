a737943b89b1950f68d2603c54bd19089494dcdd
Jin Park <jieun.park.e@deliveryhero.com>
2025-08-05 09:08:09 +0200

    fix: remove unnecessary `Transactional` annotation

--- src/test/kotlin/ecommerce/endtoend/CartApiE2ETest.kt
+++ src/test/kotlin/ecommerce/endtoend/CartApiE2ETest.kt
@@ -12,12 +12,10 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Transactional
 class CartApiE2ETest {
     @LocalServerPort
     private var port: Int = 0
